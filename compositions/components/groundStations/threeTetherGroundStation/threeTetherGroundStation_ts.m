2a4db79228ea7cfaa99f0beef895be950e7e0894
Mitchell <mcobb@ncsu.edu>
2019-05-22 20:31:21 -0400

    Ground station modularization complete.  Integrated into main plant composition ayazPlant_cl

--- compositions/components/groundStations/threeTetherGroundStation/threeTetherGroundStation_ts.m
+++ compositions/components/groundStations/threeTetherGroundStation/threeTetherGroundStation_ts.m
@@ -2,6 +2,8 @@ close all
 clear
 clc
 
+position = [1 0 0];
+
 spinEnable = 1;
 
 ten1 = 1000*[-1 0 0];
@@ -15,7 +17,7 @@ thr1Attach = [0 1 0];
 thr2Attach = [1 0 0];
 thr3Attach = [0 -1 0];
 
-initPos = 10*pi/180;
+initPos = 0*pi/180;
 initVel = 0;
 
 
@@ -24,3 +26,4 @@ sim('threeTetherGroundStation_th')
 tsc = parseLogsout;
 
 tsc.platformAngle.plot
+thr1Pos.plot
---
+++ development/groundStationVerification/groundStationVerify_ts.m
@@ -0,0 +1,129 @@
+% Test script to test modularized modle
+close all;clear;clc
+format compact
+
+% Initialize the highest level model
+OCTModel_init
+
+% Initialize all of ayaz's parameters
+ayazPlant_init
+
+% Create the bus objects in the workspace
+createAyazPlantBus
+createAyazFlowEnvironmentBus
+createaAyazCtrlBus
+
+% Set simulation duration
+duration_s = 1000;
+
+% Set active variants
+ENVIRONMENT = 'ayazFlow';
+CONTROLLER  = 'ayazController';
+PLANT       = 'ayazPlant';
+
+% Setup setpoint timeseries
+time = 0:0.1:duration_s;
+set_pitch = set_pitch*ones(size(time));
+set_roll = 20*(pi/180)*sign(sin(2*pi*time./200));
+set_roll(time<200) = 0;
+set_alt = set_alti*ones(size(time));
+
+set_pitch = timeseries(set_pitch,time);
+set_roll  = timeseries(set_roll, time);
+set_alt   = timeseries(set_alt,time);
+
+
+
+try
+sim('origionalPlant_th')
+catch
+end
+tscAyaz = parseLogsout;
+
+try
+sim('groundStationVerify_th')
+catch
+end
+tscMod = parseLogsout;
+
+%%
+close all
+figure
+subplot(3,1,1)
+plot(tscAyaz.posVec.Time,tscAyaz.posVec.Data(:,1),'LineWidth',2)
+hold on
+plot(tscMod.posVec.Time,tscMod.posVec.Data(:,1),'LineWidth',2,'LineStyle','--')
+xlabel('Time [s]')
+ylabel('x pos [m]')
+
+subplot(3,1,2)
+plot(tscAyaz.posVec.Time,tscAyaz.posVec.Data(:,2),'LineWidth',2)
+hold on
+plot(tscMod.posVec.Time,tscMod.posVec.Data(:,2),'LineWidth',2,'LineStyle','--')
+xlabel('Time [s]')
+ylabel('y pos [m]')
+
+subplot(3,1,3)
+plot(tscAyaz.posVec.Time,tscAyaz.posVec.Data(:,3),'LineWidth',2)
+hold on
+plot(tscMod.posVec.Time,tscMod.posVec.Data(:,3),'LineWidth',2,'LineStyle','--')
+xlabel('Time [s]')
+ylabel('z pos [m]')
+
+set(findall(gcf,'Type','axes'),'FontSize',24)
+
+figure
+subplot(3,1,1)
+plot(tscAyaz.velocityVec.Time,tscAyaz.velocityVec.Data(:,1),'LineWidth',2)
+hold on
+plot(tscMod.velocityVec.Time,tscMod.velocityVec.Data(:,1),'LineWidth',2,'LineStyle','--')
+xlabel('Time [s]')
+ylabel('x vel [m]')
+
+subplot(3,1,2)
+plot(tscAyaz.velocityVec.Time,tscAyaz.velocityVec.Data(:,2),'LineWidth',2)
+hold on
+plot(tscMod.velocityVec.Time,tscMod.velocityVec.Data(:,2),'LineWidth',2,'LineStyle','--')
+xlabel('Time [s]')
+ylabel('y vel [m]')
+
+subplot(3,1,3)
+plot(tscAyaz.velocityVec.Time,tscAyaz.velocityVec.Data(:,3),'LineWidth',2)
+hold on
+plot(tscMod.velocityVec.Time,tscMod.velocityVec.Data(:,3),'LineWidth',2,'LineStyle','--')
+xlabel('Time [s]')
+ylabel('z vel [m]')
+
+set(findall(gcf,'Type','axes'),'FontSize',24)
+
+figure
+subplot(3,1,1)
+plot(tscAyaz.eulerAngles.Time,tscAyaz.eulerAngles.Data(:,1)*180/pi,'LineWidth',2)
+hold on
+plot(tscMod.eulerAngles.Time,squeeze(tscMod.eulerAngles.Data(:,1))*180/pi,'LineWidth',2,'LineStyle','--')
+xlabel('Time [s]')
+ylabel('Roll [deg]')
+
+subplot(3,1,2)
+plot(tscAyaz.eulerAngles.Time,tscAyaz.eulerAngles.Data(:,2)*180/pi,'LineWidth',2)
+hold on
+plot(tscMod.eulerAngles.Time,squeeze(tscMod.eulerAngles.Data(:,2))*180/pi,'LineWidth',2,'LineStyle','--')
+xlabel('Time [s]')
+ylabel('Pitch [deg]')
+
+subplot(3,1,3)
+plot(tscAyaz.eulerAngles.Time,tscAyaz.eulerAngles.Data(:,3)*180/pi,'LineWidth',2)
+hold on
+plot(tscMod.eulerAngles.Time,squeeze(tscMod.eulerAngles.Data(:,3))*180/pi,'LineWidth',2,'LineStyle','--')
+xlabel('Time [s]')
+ylabel('Yaw [deg]')
+
+set(findall(gcf,'Type','axes'),'FontSize',24)
+
+
+
+
+
+
+
+
