01f05df5b87535ae4bf71f7a0d54259d0ec5952a
Mitchell <mcobb@ncsu.edu>
2019-05-28 17:48:18 -0400

    Comitting debugged tether.  I think this might be the last one for now. -MC

---
+++ compositions/components/singleTether/singleTether_init.m
@@ -0,0 +1,12 @@
+% for ii = 2:N
+%     initNodePoss(ii-1,:) = (ini_Rn_o(:)' - ini_R1_o(:)')*(ii-1)/(N-1);
+% end
+% initNodePoss = [ini_R1_o(:)';initNodePoss;ini_Rn_o(:)'];
+% initNodeVels = zeros(size(initNodePoss));
+
+
+initNodePoss = [linspace(ini_R1_o(1),ini_Rn_o(1),N);...
+    linspace(ini_R1_o(2),ini_Rn_o(2),N);...
+    linspace(ini_R1_o(3),ini_Rn_o(3),N)]';
+initNodeVels = zeros(size(initNodePoss));
+% x = 1;
