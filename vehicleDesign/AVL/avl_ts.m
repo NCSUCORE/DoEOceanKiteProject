d4099b785cbf054def568e71d7ad501e6d8e8500
Mitchell <mcobb@ncsu.edu>
2019-05-30 16:57:47 -0400

    Made a generic function outputs avl results for a given input file and operating conditions. -AS

---
+++ vehicleDesign/AVL/avl_ts.m
@@ -0,0 +1,15 @@
+clear
+clc
+
+ip_file_name = 'test';
+result_file_name = 'res_ft';
+
+alpha = 5;
+beta = 0;
+flap = 0;
+aileron = 0;
+elevator = 0;
+rudder = 0;
+
+
+calc_ft(ip_file_name,result_file_name,alpha,beta,flap,aileron,elevator,rudder)
