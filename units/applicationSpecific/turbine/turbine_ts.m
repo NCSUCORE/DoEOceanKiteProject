85b37e042e8a4ff14d93335f143e14437b3bf5d5
Mitchell <mcobb@ncsu.edu>
2019-05-31 12:26:26 -0400

    Unit tested turbine model. -MC

--- units/applicationSpecific/turbine/turbine_ts.m
+++ units/applicationSpecific/turbine/turbine_ts.m
@@ -4,14 +4,42 @@ d = 1;
 Cp = 0.5;
 Cd = 0.7;
 rho = 1000;
-turbPosVec = [0 1 0];
+turbPosVec = [0 -1 0];
+
+vWindBdy  = [0 0 0];
+velVecBdy = [0 0 0];
+angVecBdy = [0 0 -1];
 
 
 sim('turbine_th')
 
-simout.data(end)
+fprintf('FDragBdy\n')
+FDrag = simout.data
+uDrag = FDrag./norm(FDrag);
 
-simout1.data(end)
+fprintf('MBdy\n')
+MBdy = simout1.data
+uMBdy = MBdy./norm(MBdy);
 
-simout2.data(end)
+fprintf('power\n')
+simout2.data
 
+
+figure('Position',[ 0    0.0370    1.0000    0.8917])
+
+scatter3(0,0,0,'Marker','o','CData',[1 0 0])
+hold on
+plot3([0 turbPosVec(1)],[0 turbPosVec(2)],[0 turbPosVec(3)])
+scatter3(turbPosVec(1),turbPosVec(2),turbPosVec(3),'Marker','o','CData',[0 0 1])
+quiver3(turbPosVec(1),turbPosVec(2),turbPosVec(3),uDrag(1),uDrag(2),uDrag(3),...
+    'MaxHeadSize',10000,'LineStyle','-','Color','g')
+
+plot3([0 1],[0 0],[0 0],'LineStyle','-','Color','r')
+plot3([0 0],[0 1],[0 0],'LineStyle','-','Color','r')
+plot3([0 0],[0 0],[0 1],'LineStyle','-','Color','r')
+
+quiver3(turbPosVec(1),turbPosVec(2),turbPosVec(3),uMBdy(1),uMBdy(2),uMBdy(3),...
+    'MaxHeadSize',10000,'LineStyle','-','Color','b')
+
+
+axis square
