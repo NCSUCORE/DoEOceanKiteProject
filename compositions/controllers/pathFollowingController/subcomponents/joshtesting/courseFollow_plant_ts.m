1d992f926a70b9cd9fd520046b4cfc8a63858f35
jldanie5 <jldanie5@VermillionLab7.mae.ncsu.edu>
2019-06-07 18:37:22 -0400

    -JLD

--- compositions/controllers/pathFollowingController/subcomponents/joshtesting/courseFollow_plant_ts.m
+++ compositions/controllers/pathFollowingController/subcomponents/joshtesting/courseFollow_plant_ts.m
@@ -1,10 +1,14 @@
+clear all
+close all
+
 init_pos = [2;1;1;]/sqrt(6);
-end_time = 30;
+end_time = 100;
 aB=1;
 bB=1;
 phi_curve=.5;
 
-simWithMonitor('courseFollow_plant_th')
+sim('courseFollow_plant_th')
+% simWithMonitor('courseFollow_plant_th')
 
 lamda=@(s) aB*sin(s)./(1+(aB/bB)^2*cos(s).^2);
 phi=@(s) (aB/bB)^2*sin(s).*cos(s)./(1 + (aB/bB)^2*cos(s).^2);
@@ -16,20 +20,24 @@ a=parseLogsout;
 close all
 figure
 ax=axes;
-[x, y, z] = sphere;
-h = surfl(x, y, z); 
-set(h, 'FaceAlpha', 0.5)
-shading(ax,'interp')
+% [x, y, z] = sphere;
+% h = surfl(x, y, z); 
+% set(h, 'FaceAlpha', 0.5)
+% shading(ax,'interp')
+for i=1:length(a.pos.Data(:,1))
+plot3(a.pos.Data(1:i,1),a.pos.Data(1:i,2),a.pos.Data(1:i,3))
 hold on
-plot3(a.pos.Data(:,1),a.pos.Data(:,2),a.pos.Data(:,3))
 pathvals=path(0:.01:2*pi);
 plot3(pathvals(1,:),pathvals(2,:),pathvals(3,:),'lineWidth',3)
-% view(80,30)
-scatter3(a.star_pos.Data(:,1),a.star_pos.Data(:,2),a.star_pos.Data(:,3),'k')
-% quiver3(a.pos.Data(:,1),a.pos.Data(:,2),a.pos.Data(:,3),a.vel_des.Data(:,1),a.vel_des.Data(:,2),a.vel_des.Data(:,3))
+view(90,30)
+scatter3(a.star_pos.Data(1:i,1),a.star_pos.Data(1:i,2),a.star_pos.Data(1:i,3),'k')
+hold off
+pause(.05)
+end
+% quiver3(a.pos.Data(:,1),a.pos.Data(:,2),a.pos.Data(:,3),a.machineout.Data(:,1),a.machineout.Data(:,2),a.machineout.Data(:,3))
 % % test=a.star_pos.Data-a.vel_des.Data;
 % quiver3(a.pos.Data(:,1),a.pos.Data(:,2),a.pos.Data(:,3),a.vel.Data(:,1),a.vel.Data(:,2),a.vel.Data(:,3))
-
+%
 figure
 plot(a.central_angle)
 % 
