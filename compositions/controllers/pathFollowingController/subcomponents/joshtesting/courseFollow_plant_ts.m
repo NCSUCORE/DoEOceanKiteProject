e0c4962c3d35d44914da428475882f82344e6750
James Reed <James Reed@DESKTOP-CSUUC46>
2019-06-10 20:46:58 -0400

    -Edited courseFollw_th98

--- compositions/controllers/pathFollowingController/subcomponents/joshtesting/courseFollow_plant_ts.m
+++ compositions/controllers/pathFollowingController/subcomponents/joshtesting/courseFollow_plant_ts.m
@@ -1,7 +1,7 @@
 clear all
 % close all
- l = -1;
- p = .8;
+ l = .1;
+ p = -.53;
 path7 = [ cos(l).*cos(p);
          sin(l).*cos(p);
          sin(p);];
@@ -13,7 +13,7 @@ aB=1;
 bB=1;
 phi_curve=.5;
 
-sim('courseFollow_plant_th')
+sim('courseFollow_plant_th99')
 % simWithMonitor('courseFollow_plant_th')
 
 lamda=@(s) aB*sin(s)./(1+(aB/bB)^2*cos(s).^2);
@@ -23,13 +23,14 @@ path = @(s)[cos(lamda(s)).*cos(phi_curve+phi(s));...
             sin(phi_curve+phi(s));];
 a=parseLogsout;
 
-% close all
-% figure
-% ax=axes;
+close all
+ figure
+ ax=axes;
 [x, y, z] = sphere;
 h = surfl(x, y, z); 
 set(h, 'FaceAlpha', 0.5)
 shading(ax,'interp')
+hold on 
 for i=1:length(a.pos.Data(:,1))
 plot3(a.pos.Data(1:i,1),a.pos.Data(1:i,2),a.pos.Data(1:i,3))
 hold on
@@ -37,7 +38,7 @@ pathvals=path(0:.01:2*pi);
 plot3(pathvals(1,:),pathvals(2,:),pathvals(3,:),'lineWidth',3)
 view(90,30)
 scatter3(a.star_pos.Data(1:i,1),a.star_pos.Data(1:i,2),a.star_pos.Data(1:i,3),'k')
-hold off
+
 pause(.05)
 end
 % quiver3(a.pos.Data(:,1),a.pos.Data(:,2),a.pos.Data(:,3),a.machineout.Data(:,1),a.machineout.Data(:,2),a.machineout.Data(:,3))
