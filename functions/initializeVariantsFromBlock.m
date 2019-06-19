8d770f4a1bba4ada0cef28f63b9e86808c5a4b07
Mitchell <mcobb@ncsu.edu>
2019-06-19 18:59:59 -0400

    Cleaned up handling of variant subsystem initialization for components in plant. -MC

--- functions/initializeVariantsFromBlock.m
+++ functions/initializeVariantsFromBlock.m
@@ -1,29 +1,17 @@
 function initializeVariantsFromBlock(blkPath)
 % Determine which component we're working with
 componentName = unique(regexpi(blkPath,'vehicle|tethers|groundStation|winch','match'));
-switch lower(componentName{1})
-    case 'vehicle'
-        componentName = 'VHCL';
-        controlName   = 'VEHICLE';
-    case 'tethers'
-        componentName = 'THRS';
-        controlName   = 'TETHERS';
-    case 'groundstation'
-        componentName = 'GDST';
-        controlName   = 'GROUNDSTATION';
-    case 'winch'
-        componentName = 'WNCH';
-        controlName   = 'WINCHES';
-end
+controlName = upper(componentName{1});
 % Get variants
 vars = get_param(blkPath,'Variants');
 for ii = 1:length(vars)
     % Get the name of the block as it appears in the variant subsystem
     varName = strsplit(vars(ii).BlockName,'/');
+    subSysName = genvarname(varName{end-1});
     varName = varName{end};
     blckName = varName;
-    varName(1) = lower(varName(1));
-    variantName = genvarname(['VSS_' componentName '_' varName]);
+    varName(1) = genvarname(lower(varName(1)));
+    variantName = ['VSS_' subSysName '_' varName];
     evalin('base',sprintf('%s = Simulink.Variant(''strcmpi(%s,''''%s'''')'');',variantName,controlName,blckName));
 end
 end
