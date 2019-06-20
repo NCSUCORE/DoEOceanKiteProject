bfef647c1f1ad648bda52dbe6564bc065e23f34a
jldanie5 <jldanie5@VermillionLab7.mae.ncsu.edu>
2019-06-20 16:31:41 -0400

    Small Changes to PathFollowingController, its subsystems, and all 3 dummy plants -JLD

--- compositions/controllers/pathFollowingController/subcomponents/momentTest/momentTest_ts.m
+++ compositions/controllers/pathFollowingController/subcomponents/momentTest/momentTest_ts.m
@@ -3,7 +3,7 @@ createConstantUniformFlowEnvironmentBus
 createTestUntilRollCtrlBus
 %%%%%%%%% Env and Sim Params %%%%%%%%
 flow=[1.5;0;0;];
-sim_time=25;
+sim_time=40;
 %%%%%%%%% Plant Attributes %%%%%%%%%%
 mass = 6182; %kgs
 tetherLength = 50; %meters
@@ -12,13 +12,13 @@ velMag= 7;%m/s from sim used for proposal
 accMag= tetherTen/mass; %assumes lift = tether tension
 MOI_X=1e5; %kg*m^2 from sim used for proposal
 
-long = .05;
-lat = .51;
+long = -.2;
+lat = .56;
 path_init = tetherLength*[cos(long).*cos(lat);
          sin(long).*cos(lat);
          sin(lat);];
 initPos = [path_init(1);path_init(2);path_init(3);];
-initVelAng = 180;%degrees
+initVelAng = 45;%degrees
 initVelTan= velMag*[cosd(initVelAng);sind(initVelAng);0];
 %%%%%%%%%Controller Params%%%%%%
 aBooth=1;bBooth=1;latCurve=.5;
@@ -44,50 +44,50 @@ sim('momentTest_th')
 
 a=parseLogsout;
 % close all
-figure
-ax=axes;
-pathvals=tetherLength*boothSToGroundPos(0:.01:2*pi,aBooth,bBooth,latCurve,0);
-plot3(pathvals(1,:),pathvals(2,:),pathvals(3,:),'lineWidth',.5)
-hold on
-
-plot3(a.positionVec.Data(:,1),a.positionVec.Data(:,2),a.positionVec.Data(:,3),'lineWidth',2)
-[x,y,z]=sphere;x=tetherLength*x;y=tetherLength*y;z=tetherLength*z;
-h=surfl(x,y,z);set(h,'FaceAlpha',0.5);shading(ax,'interp')
-if min(a.positionVec.Data(:,3))>0
-    zlim([0 inf])
-end
-view(100,45)
-%
-pause(3)
-% %%
 % figure
 % ax=axes;
-% runtime=10;
-% waittime=.05;
 % pathvals=tetherLength*boothSToGroundPos(0:.01:2*pi,aBooth,bBooth,latCurve,0);
-% % filename="Dummy_Controller_2_lowaccel.gif";
-% for i=1:floor(length(a.positionVec.Data(:,1))/(runtime/waittime)):length(a.positionVec.Data(:,1))
 % plot3(pathvals(1,:),pathvals(2,:),pathvals(3,:),'lineWidth',.5)
 % hold on
-% plot3(a.positionVec.Data(1:i,1),a.positionVec.Data(1:i,2),a.positionVec.Data(1:i,3),'lineWidth',2)
-% title(['T=' num2str(a.positionVec.Time(i))])
+% 
+% plot3(a.positionVec.Data(:,1),a.positionVec.Data(:,2),a.positionVec.Data(:,3),'lineWidth',2)
 % [x,y,z]=sphere;x=tetherLength*x;y=tetherLength*y;z=tetherLength*z;
 % h=surfl(x,y,z);set(h,'FaceAlpha',0.5);shading(ax,'interp')
 % if min(a.positionVec.Data(:,3))>0
 %     zlim([0 inf])
 % end
 % view(100,45)
+% %
+% pause(3)
+%%
+figure
+ax=axes;
+runtime=10;
+waittime=.05;
+pathvals=tetherLength*boothSToGroundPos(0:.01:2*pi,aBooth,bBooth,latCurve,0);
+filename="Dummy_Controller_3";
+for i=1:floor(length(a.positionVec.Data(:,1))/(runtime/waittime)):length(a.positionVec.Data(:,1))
+plot3(pathvals(1,:),pathvals(2,:),pathvals(3,:),'lineWidth',.5)
+hold on
+plot3(a.positionVec.Data(1:i,1),a.positionVec.Data(1:i,2),a.positionVec.Data(1:i,3),'lineWidth',2)
+title(['T=' num2str(a.positionVec.Time(i))])
+[x,y,z]=sphere;x=tetherLength*x;y=tetherLength*y;z=tetherLength*z;
+h=surfl(x,y,z);set(h,'FaceAlpha',0.5);shading(ax,'interp')
+if min(a.positionVec.Data(:,3))>0
+    zlim([0 inf])
+end
+view(100,45)
 % scatter3(tetherLength*a.star_pos.Data(1:i,1),tetherLength*a.star_pos.Data(1:i,2),tetherLength*a.star_pos.Data(1:i,3),'k')
-% hold off
+hold off
 % pause(waittime)
-% %                         % Capture the plot as an image 
-% %                         frame = getframe(ax); 
-% %                         im = frame2im(frame); 
-% %                         [imind,cm] = rgb2ind(im,256); 
-% %                         % Write to the GIF File 
-% %                         if i == 1 
-% %                           imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
-% %                         else 
-% %                           imwrite(imind,cm,filename,'gif','DelayTime',waittime,'WriteMode','append'); 
-% %                         end 
-% end
+                        % Capture the plot as an image 
+                        frame = getframe(ax); 
+                        im = frame2im(frame); 
+                        [imind,cm] = rgb2ind(im,256); 
+                        % Write to the GIF File 
+                        if i == 1 
+                          imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
+                        else 
+                          imwrite(imind,cm,filename,'gif','DelayTime',waittime,'WriteMode','append'); 
+                        end 
+end
