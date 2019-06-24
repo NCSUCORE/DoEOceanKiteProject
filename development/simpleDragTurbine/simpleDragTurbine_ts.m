a91013a89e68d416736f2397e2d50f712fc621ba
Mitchell <mcobb@ncsu.edu>
2019-06-24 12:35:45 -0400

    Adding file for Josh. -MC

--- development/simpleDragTurbine/simpleDragTurbine_ts.m
+++ development/simpleDragTurbine/simpleDragTurbine_ts.m
@@ -8,19 +8,21 @@ turbine(1).Rturb_cm = [0;10;0];
 turbine(1).powerCoeff = 0.5;
 turbine(1).dragCoeff = 0.8;
 turbine(1).diameter = 1;
+turbine(1).dragFrcDirUVec = [1 0 0];
 
-% turbine(2).Rturb_cm = [0;10;0];
-% turbine(2).powerCoeff = 0.5;
-% turbine(2).dragCoeff = 0.8;
-% turbine(2).diameter = 1;
+turbine(2).Rturb_cm = [0;10;0];
+turbine(2).powerCoeff = 0.5;
+turbine(2).dragCoeff = 0.8;
+turbine(2).diameter = 1;
+turbine(2).dragFrcDirUVec = [1 0 0];
 
-turbine = reshape(turbine,1,[]);
-turbine = struct('Rturb_cm',[],'powerCoeff',[],'dragCoeff',[],'diameter',[]);
+% turbine = reshape(turbine,1,[]);
+% turbine = struct('Rturb_cm',[],'powerCoeff',[],'dragCoeff',[],'diameter',[]);
 
 % other params
 V_flow = [1;0;0];
 Vbody = [0;0;0];
-euler = [0;0;0];
+angVel = [0;0;0];
 
 rho_fluid = 1;
 
