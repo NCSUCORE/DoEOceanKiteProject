c3e824707a4bc7edabb0601539ddf835de3e1cef
Mitchell <mcobb@ncsu.edu>
2019-06-24 12:05:27 -0400

    Created unit for local apparent wind. Still need to add to fluid dynamic surface  -MC

--- development/simpleDragTurbine/simpleDragTurbine_ts.m
+++ development/simpleDragTurbine/simpleDragTurbine_ts.m
@@ -9,12 +9,13 @@ turbine(1).powerCoeff = 0.5;
 turbine(1).dragCoeff = 0.8;
 turbine(1).diameter = 1;
 
-turbine(2).Rturb_cm = [0;10;0];
-turbine(2).powerCoeff = 0.5;
-turbine(2).dragCoeff = 0.8;
-turbine(2).diameter = 1;
+% turbine(2).Rturb_cm = [0;10;0];
+% turbine(2).powerCoeff = 0.5;
+% turbine(2).dragCoeff = 0.8;
+% turbine(2).diameter = 1;
 
 turbine = reshape(turbine,1,[]);
+turbine = struct('Rturb_cm',[],'powerCoeff',[],'dragCoeff',[],'diameter',[]);
 
 % other params
 V_flow = [1;0;0];
@@ -23,7 +24,7 @@ euler = [0;0;0];
 
 rho_fluid = 1;
 
-% sim('simpleDragTurbine_th')
+sim('simpleDragTurbine_th')
 
 
 
