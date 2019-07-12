1c9d227dd314ab78dd0fd86bf74bb9ba172ee07e
RODNEY-PC\Rodney <rmmetoye@ncsu.edu>
2019-07-12 10:43:56 -0400

    Removed vehicle dependence. We want to isolate the tether for this test.

--- compositions/plant/components/tethers/tether000/tether000Test_ts.m
+++ compositions/plant/components/tethers/tether000/tether000Test_ts.m
@@ -1,55 +1,45 @@
-%% busses
+% tether000Test_ts.m
+% The purpose of this script is to test the tether model
+% The goal is to test the tether model as an independent module.
+% Try not to unintentionally rely on other model assumptions
+clearvars; close all; clc;
+
+% Simulation control parameters
+scaleFactor  = 1;
+
+%% create busses
 createThrTenVecBus
 createThrAttachPtKinematicsBus
 createConstantUniformFlowEnvironmentBus
-%%
-scaleFactor  = 1;
-vhcl = OCT.vehicle;
-vhcl.numTethers.setValue(1,'');
-vhcl.numTurbines.setValue(2,'');
-vhcl.mass.setValue(945.4,'kg')
-vhcl.volume.setValue(0.9454,'m^3');
-vhcl.build('partDsgn1_lookupTables.mat');
 
-%%
+%% Construct objects
+% environment
 env = ENV.env;
 env.addFlow({'water'},'FlowDensities',1000);
-% Set Values
 env.water.velVec.setValue([1 0 0],'m/s');
-% Scale up/down
 env.scale(scaleFactor);
 
-%%
 % ground station
 groundStation = [0;0;0];
 
 %vehicles initial position in ground frame
-vehicleInitialPositionGround =  1.0e+02 *...
-   [0.541196100146197;
-  -0.541196100146197;
-   1.847759065022573;];
-%water density
-rho = 1000;
-% gravity
-grav = 9.81;
-%vehicle initial euler angles
+
+%% Does this stuff need to be in the workspace in order for the model to run?
+% If so, I'd recommend init methods on the appropriate classes that put the
+% variables into the workspace. The whole point is to take the burden of
+% knowing what is supposed to be there off of the developers.
+vehicleInitialPositionGround = 100*[0.541196100146197;-0.541196100146197;1.847759065022573;];
 vehicleInitialEulerAngles = [-0.392699081698724  0  -2.356194490192345];
+bodyAttachmentPoint = [0;0;0]; % attachment point on the vehicle in the body frame
+vehicleInitialVelocity = [-19;0;0];  % vehicle initial velocity
+vehicleMass = 945.4; % vehicle mass
+rho = 1000;  % water density
+grav = 9.81; % gravity
+tetherLength = 200; % tether length
 
-%attachment point on the vehicle in the body frame
-bodyAttachmentPoint = [0;0;0];
-
-%vehicle initial velocity
-vehicleInitialVelocity = [-19;0;0];
-
-%vehicle mass
-vehicleMass = 9.454000000000000e+02;
-
-%variants
+%% Define Variants
 TETHERS = 'tether000';
 VARIANTSUBSYSTEM = 'NNodeTether';
-
-%tether length
-tetherLength = 200;
 
 thr = OCT.tethers;
 thr.setNumTethers(1,'');
@@ -72,9 +62,7 @@ createThrNodeBus(thr.numNodes.Value);
 thr.tether1.setDragEnable(true,'');
 thr.tether1.setSpringDamperEnable(true,'');
 thr.tether1.setNetBuoyEnable(true,'');
-
-
-thr.designTetherDiameter(vhcl,env);
+thr.tether1.diameter.setValue(0.0144,'m');
 
 % Scale up/down
 thr.scale(scaleFactor);
@@ -120,32 +108,32 @@ lat1 = radius.*(latCurve+sin(x)); %path latitude
 positionTopNode = tetherLength *  [ cos(long1).*cos(lat1);
                   sin(long1).*cos(lat1);
                   sin(lat1);];
-     %% simulate
-radialVelocityBit = 1; 
 
+radialVelocityBit = 1; 
+constantVelocity = 1;
+
+%% simulate
 sim('tether000Test1')
 
     
-%%
+%% Viz Results
+
 %plot sphere
 theta = linspace(0, 2*pi);
 phi = linspace(-pi/2 , pi/2); 
 [theta, phi] = meshgrid(theta, phi); 
 rho = 1; 
-
 [x,y,z] = sph2cart(theta, phi, rho);
 surf(x,y,z)
-%% tangent to path Graphic
-s = linspace(0,2*pi, 100);
 
+% tangent to path Graphic
+s = linspace(0,2*pi, 100);
 plot3(positionTopNode(1,:),positionTopNode(2,:), positionTopNode(3,:))
 hold on 
-
 theta = linspace(0, 2*pi);
 phi = linspace(-pi/2 , pi/2); 
 [theta, phi] = meshgrid(theta, phi); 
 rho = 200; 
-
 %[x,y,z] = sph2cart(theta, phi, rho);
 %surf(x,y,z)
 hold on
