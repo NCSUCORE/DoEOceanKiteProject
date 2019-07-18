9a40bb02ed2e629cbd5f45c846bc066ff537d488
jldanie5 <jldanie5@VermillionLab7.mae.ncsu.edu>
2019-07-18 17:30:26 -0400

    pathgeometry change and more work on the test script -JLD

--- functions/pathGeometryFunctions/swapablePath.m
+++ functions/pathGeometryFunctions/swapablePath.m
@@ -1,9 +1,7 @@
 function [posGround,varargout] = swapablePath(pathVariable,geomParams)
-%I hate that this is what I came up with, but it should work so...ugh.
-    func =  @circleOnSphere;
-    posGround = func(pathVariable,geomParams);
-    if nargout == 2
-        [~,varargout{1}] = func(pathVariable,geomParams);
-    end
-        
+     func = @circleOnSphere;
+     posGround = func(pathVariable,geomParams);
+     if nargout == 2
+          [~,varargout{1}] = func(pathVariable,geomParams);
+     end
 end
