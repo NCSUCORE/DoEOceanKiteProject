a7ced62a8ec59389345764209793bc548af0a2fb
Erin Beaton <embeato2@ncsu.edu>
2019-07-19 18:21:20 -0400

    implemented winches and an early version of a sway,surge,heave controller on floating platform -EB

--- compositions/plant/components/groundStations/groundStation001/groundStation001_ts.m
+++ compositions/plant/components/groundStations/groundStation001/groundStation001_ts.m
@@ -6,7 +6,7 @@ load('pos.mat')
 % clear
 
 % run this to set magnitude to zero %
-mag.Data = zeros
+mag.Data = zeros;
 
 % % run this to set magnitude to average magnitude %
 % mag.Data = mean(mag.Data)*ones(size(mag.Data));
@@ -35,14 +35,27 @@ platformInertiaMatrix = ((1/6)*platformMass*objectHeight^2).*eye(3);
 
 % initial conditions
 initPos = [0 0 100];
-initVel = [0 0 0];
+initVel = [1e-3 0 0];
 initEulAng = (pi/180).*[0 0 0];
 initAngVel = [0 0 0];
+
+%
+vhcl = OCT.vehicle;
+vhcl.mass.setValue(platformMass,'kg');
+vhcl.volume.setValue(platformVolume,'m^3');
+vhcl.Ixx.setValue(platformInertiaMatrix(1,1),'kg*m^2');
+vhcl.Iyy.setValue(platformInertiaMatrix(2,2),'kg*m^2');
+vhcl.Izz.setValue(platformInertiaMatrix(3,3),'kg*m^2');
+vhcl.Ixy.setValue(0,'kg*m^2');
+vhcl.Ixz.setValue(0,'kg*m^2');
+vhcl.Iyz.setValue(0,'kg*m^2');
+vhcl.setICs('InitPos',initPos,'InitEulAng',initEulAng);
+vhcl.centOfBuoy.setValue([0 0 objectHeight/4],'m');
 
 % environmental properties
 env = ENV.env;
 env.addFlow({'water'},'FlowDensities',1000);
-env.water.velVec.setValue([1 0 0],'m/s');
+env.water.velVec.setValue([0 0 0],'m/s');
 grav = env.gravAccel.Value;
 rho = env.water.density.Value;
 
@@ -56,10 +69,11 @@ anchThrPltfrm(1).posVec = [0 1 0]';
 anchThrPltfrm(2).posVec = [cosd(30) -.5 0]';
 anchThrPltfrm(3).posVec = [-cosd(30) -.5 0]';
 
+dist = 100;
 % Vector from ground fixed origin to tether attachment point, in ground frame
-anchThrGnd(1).posVec = 100.*anchThrPltfrm(1).posVec;
-anchThrGnd(2).posVec = 100.*anchThrPltfrm(2).posVec;
-anchThrGnd(3).posVec = 100.*anchThrPltfrm(3).posVec;
+anchThrGnd(1).posVec = dist.*anchThrPltfrm(1).posVec;
+anchThrGnd(2).posVec = dist.*anchThrPltfrm(2).posVec;
+anchThrGnd(3).posVec = dist.*anchThrPltfrm(3).posVec;
 
 % (theoretical) tether attachment points for a lifting body on platform
 airbThrPltfrm(1).posVec = [0 1/2 1/2]';
@@ -117,8 +131,78 @@ thrs.tether3.setSpringDamperEnable(true,'');
 thrs.tether3.setNetBuoyEnable(true,'');
 tetherLengths(3) = norm(thrs.tether3.initAirNodePos.Value-thrs.tether3.initGndNodePos.Value);
 
+wins = OCT.winches;
+wins.setNumWinches(3,'');
+wins.build;
+wins.setTetherInitLength(vhcl,env,thrs);
+
+wins.winch1.maxSpeed.setValue(0.4,'m/s');
+wins.winch1.timeConst.setValue(1,'s');
+wins.winch1.maxAccel.setValue(inf,'m/s^2')
+
+wins.winch2.maxSpeed.setValue(0.4,'m/s');
+wins.winch2.timeConst.setValue(1,'s');
+wins.winch2.maxAccel.setValue(inf,'m/s^2')
+
+wins.winch3.maxSpeed.setValue(0.4,'m/s');
+wins.winch3.timeConst.setValue(1,'s');
+wins.winch3.maxAccel.setValue(inf,'m/s^2')
+
+ctrl = CTR.controller;
+ctrl.add('FPIDNames',{'surge','sway','heave'},...
+    'FPIDErrorUnits',{'m','m','m'},...
+    'FPIDOutputUnits',{'m/s','m/s','m/s'});
+ctrl.add('GainNames',{'thrAllocationMat'},...
+    'GainUnits',{'1/s'});
+ctrl.add('SaturationNames',{'outputSat'});
+ctrl.add('SetpointNames',{'surgeSP','swaySP','heaveSP'},...
+    'SetpointUnits',{'m','m','m'});
+
+ctrl.surge.kp.setValue(1,'(m/s)/(m)');
+ctrl.surge.kd.setValue(0*ctrl.surge.kp.Value,'(m/s)/(m/s)');
+ctrl.surge.tau.setValue(.1,'s');
+
+ctrl.sway.kp.setValue(1,'(m/s)/(m)');
+ctrl.sway.kd.setValue(0*ctrl.sway.kp.Value,'(m/s)/(m/s)');
+ctrl.sway.tau.setValue(.1,'s');
+
+ctrl.heave.kp.setValue(1,'(m/s)/(m)');
+ctrl.heave.kd.setValue(0*ctrl.heave.kp.Value,'(m/s)/(m/s)');
+ctrl.heave.tau.setValue(.1,'s');
+
+ctrl.outputSat.upperLimit.setValue(.4,'');
+ctrl.outputSat.lowerLimit.setValue(-.4,'');
+
+timeV = 0:.1:sim_time;
+ctrl.surgeSP.Value = timeseries(0*ones(size(timeV)),timeV);
+% ctrl.surgeSP.Value.DataInfo.Units = 'm';
+ctrl.swaySP.Value = timeseries(0*ones(size(timeV)),timeV);
+% ctrl.surgeSP.Value.DataInfo.Units = 'm';
+ctrl.heaveSP.Value = timeseries(100*ones(size(timeV)),timeV);
+% ctrl.surgeSP.Value.DataInfo.Units = 'm';
+
+surge1 = -dist + sqrt(dist^2+1);
+surge1 = 0
+surge2 = -dist + sqrt(dist^2+1-2*dist*cosd(30));
+surge3 = -dist + sqrt(dist^2+1-2*dist*cosd(120));
+surgeVec = [0 0 0]';
+% surgeVec = [surge1 surge2 surge3]';
+% surgeVec = surgeVec/norm(surgeVec);
+swayNum = -dist + sqrt(dist^2+1^2-2*dist*cosd(60));
+swayVec = [0 0 0]';
+% swayVec = [-1 -swayNum -swayNum]';
+% swayVec = swayVec/norm(swayVec);
+heaveNum = -sqrt(mean(ctrl.heaveSP.Value.Data)^2+dist^2) + sqrt((mean(ctrl.heaveSP.Value.Data)+1)^2+dist^2);
+heaveVec = [0 0 0]';
+heaveVec = [heaveNum heaveNum heaveNum]';
+heaveVec = heaveVec/norm(heaveVec);
+
+% ctrl.thrAllocationMat.setValue(-1.*[surge1 surge2 surge3;-1 -swayNum -swayNum;-heaveNum -heaveNum -heaveNum]','1/s')
+ctrl.thrAllocationMat.setValue([surgeVec,swayVec,heaveVec],'1/s')
+ctrl.thrAllocationMat.Value
+
 % circulation data
-v = 0.5;
+v = 0;
 vsquared = v^2;
 cd = .8;
 A = platformVolume^(2/3);
@@ -139,30 +223,37 @@ parseLogsout
 %%
 close all
 
+parseLogsout
 figure
 oH = oceanDepth + waveAmp*sin(2*pi/(wavePeriod).*tsc.subBodyPos.Time);
 diff = squeeze(tsc.subBodyPos.Data(3,:,:)) - oH;
 plot(tsc.subBodyPos.Time,diff)
 xlabel('Time, t [s]')
 ylabel('Distance from Ocean Surface [m]')
+
 % Plot position
 figure
 subplot(3,1,1)
-plot(tsc.subBodyPos.Time,squeeze(tsc.subBodyPos.Data(1,:,:)))
+plot(tsc.subBodyPos.Time,squeeze(tsc.subBodyPos.Data(1,:,:)),'k')
+hold on
+plot(timeV,squeeze(ctrl.surgeSP.Value.Data),'r')
 grid on
 xlabel('Time, t [s]')
 ylabel('X Pos [m]')
 
 subplot(3,1,2)
-plot(tsc.subBodyPos.Time,squeeze(tsc.subBodyPos.Data(2,:,:)))
+plot(tsc.subBodyPos.Time,squeeze(tsc.subBodyPos.Data(2,:,:)),'k')
+hold on
+plot(timeV,squeeze(ctrl.swaySP.Value.Data),'r')
 grid on
 xlabel('Time, t [s]')
 ylabel('Y Pos [m]')
 
 subplot(3,1,3)
-plot(tsc.subBodyPos.Time,squeeze(tsc.subBodyPos.Data(3,:,:)))
+plot(tsc.subBodyPos.Time,squeeze(tsc.subBodyPos.Data(3,:,:)),'k')
 hold on
-plot(tsc.subBodyPos.Time,oH,'r')
+plot(tsc.subBodyPos.Time,oH)
+plot(timeV,squeeze(ctrl.heaveSP.Value.Data),'r')
 grid on
 xlabel('Time, t [s]')
 ylabel('Z Pos [m]')
@@ -189,8 +280,16 @@ ylabel('Yaw, [deg]')
 
 % Animate some stuff
 
+figure
+tsc.tetherLengths.plot
+legend('1: pos y','2: pos x, neg y','3: neg x, neg y')
+
+figure
+tsc.winchSpeeds.plot
+legend('1','2','3')
+
 timeStep = 1;
-fileName = 'sub_circ.gif';
+% fileName = 'sub_circ.gif';
 
 % Resample data to the animation framerate
 timeVec = 0:timeStep:tsc.subBodyPos.Time(end);
@@ -229,12 +328,12 @@ end
 h.title = title(sprintf('Time = %d',tsc.subBodyPos.Time(1)));
 
 set(gca,'FontSize',24)
+% 
+% frame = getframe(h.fig );
+% im = frame2im(frame);
+% [imind,cm] = rgb2ind(im,256);
 
-frame = getframe(h.fig );
-im = frame2im(frame);
-[imind,cm] = rgb2ind(im,256);
-
-imwrite(imind,cm,fileName,'gif', 'Loopcount',inf);
+% imwrite(imind,cm,fileName,'gif', 'Loopcount',inf);
 
 for ii = 2:numel(timeVec)
     
@@ -255,10 +354,10 @@ for ii = 2:numel(timeVec)
     
     drawnow
     
-    frame = getframe(h.fig); 
-    im = frame2im(frame); 
-    [imind,cm] = rgb2ind(im,256);
-    imwrite(imind,cm,fileName,'gif','WriteMode','append','DelayTime',0.05);
+%     frame = getframe(h.fig); 
+%     im = frame2im(frame); 
+%     [imind,cm] = rgb2ind(im,256);
+%     imwrite(imind,cm,fileName,'gif','WriteMode','append','DelayTime',0.05);
     
 end
 
