958de3cd81d4c7585d20d0f800c63df89bee5e58
jldanie5 <jldanie5@VermillionLab7.mae.ncsu.edu>
2019-08-22 14:49:10 -0400

    fixed rebase -JLD

--- scripts/rebase.m
+++ scripts/rebase.m
@@ -5,4 +5,4 @@
 %most updated version.  This is basically in place of a git merge.  You can
 %still end up with conflicts if you changed a file that was changed since
 %the commit you actually build off.
-!git rebase master
+!git rebase origin/master
