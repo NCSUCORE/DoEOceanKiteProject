0edb140b77c99f8242db8e734892e61201789cd2
Mitchell <mcobb@ncsu.edu>
2019-10-03 17:11:48 -0400

    Cleaned some build scripts. -MC

--- compositions/controllers/flightController/pathFollowingController/library/jamesFileThatWasHere.m
+++
@@ -1,6 +0,0 @@
- ctrl.fun = @constantUniformFlow_bc;
- ctrl.fun1 = @plant_bc;
- ctrl.fun2 = @pathFollowingController_bc;
- ctrl.fun3 = @oneDoFGndStnCtrl_bc;
- ctrl.fun4 = @oneTetherThreeSurfaceCtrl_bc;
- save(fullfile(fileparts(which(mfilename)),strrep(mfilename,'_bs','')),'ctrl')
--- compositions/environment/flowCalculation/constXY_ZvarT_ADCP/library/constXY_ZvarT_ADCP_bs.m
+++ compositions/environment/flowCalculation/constXY_ZvarT_ADCP/library/constXY_ZvarT_ADCP_bs.m
@@ -1,12 +1,7 @@
-%% Set up environment
 env = ENV.env;
 env.gravAccel.setValue(9.81,'m/s^2')
 env.addFlow({'water'},{'constXY_ZvarT_ADCP'},'FlowDensities',1000)
 
-env.water = env.water.setStartADCPTime(35000,'s');
-%sets adcp time to be 100 minutes
-env.water = env.water.setEndADCPTime(38000,'s');
-% env.setFlowTSeries('')
 environment_bc
 % env.water.nominal100mFlowVec.setValue( 1.5,'m/s') 
 
