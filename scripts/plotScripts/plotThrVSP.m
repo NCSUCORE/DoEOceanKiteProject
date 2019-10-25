b5e6faf716da9dda3a7edf1b3fe40597d7e9cb31
jcreed2 <jcreed2@ncsu.edu>
2019-10-25 11:43:10 -0400

    changes to CNAPS turb for upsidown behavior

--- scripts/plotScripts/plotThrVSP.m
+++ scripts/plotScripts/plotThrVSP.m
@@ -1,10 +1,17 @@
 figure; 
-tsc.LThr.plot
+plot(tsc.LThr.time,tsc.LThr.data)
 grid on
 hold on
-tsc.LThrSP.plot
-scatter(tsc.LThrSP.Time(tsc.currentPathVar.Data<0.005),tsc.LThrSP.Data(tsc.currentPathVar.Data<0.005),'r')
+plot(tsc.LThrSP.time,tsc.LThrSP.data)
+% scatter(tsc.LThrSP.Time(tsc.currentPathVar.Data<0.005),tsc.LThrSP.Data(tsc.currentPathVar.Data<0.005),'r')
+xlim([500,800])
+
 title('Tether Length vs. Tether Length SP')
 xlabel('Time (s)')
 ylabel('Length (m)')
-legend('Tether Length','Tether Length SP',' SP at S = 0')
+legend('Tether Length','Tether Length SP') %,' SP at S = 0')
+
+set(findall(gcf,'Type','axes'),'FontSize',16)
+
+grid on
+box off
--- units/applicationSpecific/pathGeometry/pathGeometryFunctions/pathGeometryVisualizer.m
+++ units/applicationSpecific/pathGeometry/pathGeometryFunctions/pathGeometryVisualizer.m
@@ -4,12 +4,14 @@ hold on
 % pathParams = [.73,1,.37,0,125]; %Lem
 % pathParams = [.73,1.4,.36,0,125];%Circle
 % pathParams = [pi/24,3*pi/8,pi/8,0,125];%Racetrack
-pathParams = [.7,1,.41,0,125];%Ellipse
+% pathParams = [.8,1.6,-.3,0,125];%Ellipse
+
+ pathParams = [1.6,.3,-.3,0,125];%Ellipse
 % pathvals=lemOfBooth(linspace(0,1,1000),pathParams);
 % plot3(pathvals(1,:),pathvals(2,:),pathvals(3,:),'k--','lineWidth',.5)
 
  pathvals=ellipse(linspace(0,1,1000),pathParams);
- plot3(pathvals(1,:),pathvals(2,:),pathvals(3,:),'k--','lineWidth',.5)
+ plot3(pathvals(1,:),pathvals(2,:),pathvals(3,:),'lineWidth',1.4)
 % 
 % pathvals=lemOfBooth(linspace(.625,.725,1000),pathParams);
 % plot3(pathvals(1,:),pathvals(2,:),pathvals(3,:),'lineWidth',1.5)
@@ -22,7 +24,7 @@ pathParams = [.7,1,.41,0,125];%Ellipse
 tetherLength=pathParams(end);
 [x,y,z]=sphere;x=tetherLength*x;y=tetherLength*y;z=tetherLength*z;
 h=surfl(x,y,z);set(h,'FaceAlpha',0.5);shading(gca,'interp')
-view(90,15)
+view(90,37)
 ylims=ylim;
-zlim([0 ylims(2)])
+zlim([-125 0])
 hold off
