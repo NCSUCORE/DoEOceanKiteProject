b8baeaeb16e96f2c2c0af43142971bf6d658ec9e
jcreed2 <jcreed2@ncsu.edu>
2019-11-15 11:29:31 -0500

    made uf vf and wf, which is all the components for each element. Trying to figure out how to take each element and make the matrix grid. JR

--- testScripts/scratchpad.m
+++ testScripts/scratchpad.m
@@ -3,16 +3,16 @@ clear
 clc
 x = ENV.CNAPS('StartTime',8.6857e+05,'EndTime',8.6857e+05+3600*3+1);
 % x.cropGUI
-x.setXGridPoints(0:2,'m');
+x.setXGridPoints(1:1,'m');
 x.setYGridPoints(0:1:5,'m');
 
 y = ENV.FAUTurb;
 y.setIntensity(0.1,'');
 y.setMinFreqHz(0.1,'Hz');
-y.setMaxFreqHz(10,'Hz');
+y.setMaxFreqHz(1,'Hz');
 y.setNumMidFreqs(5,'');
 y.setLateralStDevRatio(0.1,'');
-y.setVerticalStDevRatio(0.01,'');
+y.setVerticalStDevRatio(0.1,'');
 y.setSpatialCorrFactor(5,'');
 y.process(x,'Verbose',true);
 
