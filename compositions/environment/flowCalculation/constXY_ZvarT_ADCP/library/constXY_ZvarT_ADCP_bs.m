f2a6ad2d534ab35a09f686c61d32da87cc84759d
Mitchell <mcobb@ncsu.edu>
2019-11-20 17:23:11 -0500

    Deleted unused flow calculations.

--- compositions/environment/flowCalculation/constXY_ZvarT_ADCP/library/constXY_ZvarT_ADCP_bs.m
+++
@@ -1,8 +0,0 @@
-env = ENV.env;
-env.gravAccel.setValue(9.81,'m/s^2')
-env.addFlow({'water'},{'constXY_ZvarT_ADCP'},'FlowDensities',1000)
-
-environment_bc
-
-FLOWCALCULATION = 'constXY_ZvarT_ADCP';
-saveBuildFile('env',mfilename,'variant','FLOWCALCULATION');
