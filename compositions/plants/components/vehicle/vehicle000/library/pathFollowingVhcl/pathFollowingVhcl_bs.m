873b2b2134b3087833c008470c619395796c72f4
Ayaz <asiddiq2@ncsu.edu>
2020-01-16 17:04:30 -0500

    made minor changes to pathFollowingVhcl_bs

--- compositions/plants/components/vehicle/vehicle000/library/pathFollowingVhcl/pathFollowingVhcl_bs.m
+++ compositions/plants/components/vehicle/vehicle000/library/pathFollowingVhcl/pathFollowingVhcl_bs.m
@@ -23,7 +23,7 @@ vhcl.setBuoyFactor(1.0,'');
 % vhcl.setMaxCtrlDefSpeed(60,'deg/s');
 
 % % % volume and inertias
-% vhcl.setVolume(945352023.474*1e-9,'m^3');
+vhcl.setVolume(945352023.474*1e-9,'m^3');
 vhcl.setIxx(6.303080401918E+09*1e-6,'kg*m^2');
 vhcl.setIyy(2080666338.077*1e-6,'kg*m^2');
 vhcl.setIzz(8.320369733598E+09*1e-6,'kg*m^2');
