b2ef2761bc7c56d2bbf68eabd2fa66db3973aec3
mkcobb <mitchell.k.cobb@gmail.com>
2020-01-22 19:10:06 -0500

    Wrote timesignal.diffMC to test differences between Josh's implementation and mine. Found minor differences (~1e-3 rad in Euler angles).  The real test will be to integrate signals and see who's matches the true result.  Will need to write a method for that tomorrow. -MC

--- testScripts/scratchpad.m
+++ testScripts/scratchpad.m
@@ -1,1 +1,5 @@
-tsc = signalcontainer(logsout,'Verbose',true);
+plot(tsc.tetherLengths.diffMC-tsc.tetherLengths.diff)
+figure
+plot(tsc.eulerAngles.diffMC-tsc.eulerAngles.diff)
+figure
+plot(tsc.BMatrix.diffMC-tsc.BMatrix.diff)
