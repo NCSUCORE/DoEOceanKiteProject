a3990a2ed22c6e70b0c12cb9e0b71ccccad2c700
mkcobb <mitchell.k.cobb@gmail.com>
2020-04-21 10:27:13 -0400

    Changed instance of writeToExcel to writetoexcel for consistency. -MC

--- classes/dataManagement/@signalcontainer/writetoexcel.m
+++ classes/dataManagement/@signalcontainer/writetoexcel.m
@@ -1,6 +1,6 @@
 function writetoexcel(obj,fileName,sigNames)
 %% Writes specified signals to excel file
 for ii = 1:numel(sigNames)
-    obj.(sigNames{ii}).writeToExcel(fileName)
+    obj.(sigNames{ii}).writetoexcel(fileName)
 end
 end
