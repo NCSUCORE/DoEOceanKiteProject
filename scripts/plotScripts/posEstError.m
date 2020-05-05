b0654df42e73b4d882d9d0f7084cdb81b554c7cd
knaik <knaik@VermillionLab8.mae.ncsu.edu>
2020-05-05 16:08:51 -0400

    Plotting script: posEstError modified

--- scripts/plotScripts/posEstError.m
+++ scripts/plotScripts/posEstError.m
@@ -16,33 +16,53 @@ kitePosZ = squeeze(kitePos(3,:,:))';
 R = zeros(1,length(midNodePosX)); 
 El = zeros(1,length(midNodePosX)); 
 Az = zeros(1,length(midNodePosX)); 
+El_est = zeros(1,length(midNodePosX)); 
+Az_est = zeros(1,length(midNodePosX)); 
 x_est = zeros(1,length(midNodePosX)); 
 y_est = zeros(1,length(midNodePosX)); 
 z_est = zeros(1,length(midNodePosX)); 
 x_err = zeros(1,length(midNodePosX)); 
 y_err = zeros(1,length(midNodePosX)); 
 z_err = zeros(1,length(midNodePosX)); 
+El_err = zeros(1,length(midNodePosX)); 
+Az_err = zeros(1,length(midNodePosX)); 
+
 
 for i = 1:length(midNodePosX)
     R(i) = sqrt(midNodePosX(i)^2 + midNodePosY(i)^2 + (200-midNodePosZ(i))^2);
+    R_k(i) = sqrt(kitePosX(i)^2 + kitePosY(i)^2 + (200-kitePosZ(i))^2);
     El(i) = asin((200-midNodePosZ(i))/R(i)); 
     Az(i) = asin(midNodePosY(i)/sqrt(midNodePosX(i)^2 + midNodePosY(i)^2)); 
+    El_est(i) = asin((200-kitePosZ(i))/R_k(i)); 
+    Az_est(i) = asin(kitePosY(i)/sqrt(kitePosX(i)^2 + kitePosY(i)^2));
     z_est(i) = tetherLength(i)*sin(El(i)); 
     x_est(i) = tetherLength(i)*cos(El(i))*cos(Az(i)); 
     y_est(i) = tetherLength(i)*cos(El(i))*sin(Az(i));
-    x_err(i) = (abs(x_est(i)-kitePosX(i)))/max(kitePosX); 
-    y_err(i) = (abs(y_est(i)-kitePosY(i)))/max(kitePosY);
-    z_err(i) = (abs((200-z_est(i))-kitePosZ(i)))/max(kitePosZ); 
+    x_err(i) = ((x_est(i)-kitePosX(i)))/max(kitePosX); 
+    y_err(i) = ((y_est(i)-kitePosY(i)))/max(kitePosY);
+    z_err(i) = (((200-z_est(i))-kitePosZ(i)))/max(kitePosZ); 
 end 
 
+for ii= 1:length(midNodePosX)
+    El_err = (El_est-El);%/max(El); 
+    Az_err = (Az_est-Az);%/max(Az); 
+end 
 figure; 
 plot(time,100*x_err)
 hold on 
 plot(time,100*y_err)
 plot(time,100*z_err)
 hold off 
-title('error(%) of position estimates using LAS') 
+title('error(%) of Angle estimates using LAS') 
 legend('X error','Y error','Z error')
+
+figure; 
+plot(time,100*El_err)
+hold on 
+plot(time,100*Az_err)
+hold off 
+title('error(%) of position estimates using LAS') 
+legend('El error','Az error')
 
 figure; 
 plot(time,kitePosX)
@@ -50,7 +70,6 @@ hold on
 plot(time,x_est,'--')
 title('x positions') 
 legend('true value','LAS Estimate')
-
 
 figure; 
 plot(time,kitePosY)
@@ -66,6 +85,25 @@ plot(time,200-z_est,'--')
 title('z positions') 
 legend('true value','LAS Estimate')
 
-mean_X_error = 100*mean(x_err) 
-mean_Y_error = 100*mean(y_err) 
-mean_Z_error = 100*mean(z_err) 
+
+max_X_error = 100*max(x_err) 
+max_Y_error = 100*max(y_err) 
+max_Z_error = 100*max(z_err) 
+max_El_error = 100*max(El_err) 
+max_Az_error = 100*max(Az_err) 
+
+%% 
+
+figure; 
+plot(time,El)
+hold on 
+plot(time,El_est,'--')
+title('Elevation') 
+legend('true value','LAS Estimate')
+
+figure; 
+plot(time,Az)
+hold on 
+plot(time,Az_est,'--')
+title('Azimuth') 
+legend('true value','LAS Estimate')
