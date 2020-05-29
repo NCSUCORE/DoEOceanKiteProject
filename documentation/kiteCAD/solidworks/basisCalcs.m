a3f0c4a141799ab2d25baeaf81984e45224b846e
Ayaz <asiddiq2@ncsu.edu>
2020-05-29 00:56:40 -0400

    made the required changes to the kite CAD with the turbine.

--- documentation/kiteCAD/solidworks/basisCalcs.m
+++ documentation/kiteCAD/solidworks/basisCalcs.m
@@ -8,3 +8,20 @@ aspectRatio = 7.5;
 taperRatio = 0.8;
 
 sweepAngle = atan(2*(1-taperRatio)/aspectRatio)*180/pi;
+
+%% turbine diameter calculation
+% kite reference area
+sKite = 10;
+% approximate kite drag coefficient
+CDkite = 0.15;
+% nominal turbine power coefficient
+CpNom = 0.5;
+% nominal turbine drag coefficient
+K = 1.5;
+CdNom = K*CpNom;
+% turbine area if, CdNom = 0.5*CDkite
+Aturb = (CDkite*sKite)/(2*CdNom);
+% turbine diameter
+dTurb = sqrt(4*Aturb/pi);
+
+
