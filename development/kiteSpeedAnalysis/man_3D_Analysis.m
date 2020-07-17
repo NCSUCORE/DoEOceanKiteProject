fe35fc7f27e95e47056c6a0d246527c625744919
Ayaz <asiddiq2@ncsu.edu>
2020-07-17 17:01:42 -0400

    made a correction in tht maneuverabilityAnalysis classdef.

--- development/kiteSpeedAnalysis/man_3D_Analysis.m
+++ development/kiteSpeedAnalysis/man_3D_Analysis.m
@@ -10,18 +10,17 @@ cIn.bBooth = 1.5;
 cIn.tetherLength = 50;
 cIn.meanElevationInRadians = 30*pi/180;
 
-res = cIn.analyseFlatEarthRes([1.5 2]);
+res = cIn.analyseFlatEarthRes([1 2]);
 
 %% calculate max achievable radius
 mass = 3e3;
 CL = 0.8;
 rho = 1e3;
 Aref = 10;
-maxTangentRollAngle = 2;
+maxTangentRollAngle = 30;
 
 staticVal = mass/(0.5*CL*rho*Aref);
 minR = staticVal/sind(maxTangentRollAngle);
 
 subplot(3,1,3)
 yline(minR,'m-','linewidth',1);
-
