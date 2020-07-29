17186a7b2ef9285330a991873f088565f9d9154b
Ayaz <asiddiq2@ncsu.edu>
2020-07-29 12:24:09 -0400

    verifying the radius of curvature calculations with a 3D path formed by the intersection of a sphere and a plane.

--- development/kiteSpeedAnalysis/man_3D.m
+++ development/kiteSpeedAnalysis/man_3D.m
@@ -1,42 +1,47 @@
 clear
 clc
+close all
 
 %% initailize
 cIn = maneuverabilityAnalysisLibrary;
-cIn.aBooth = 0.9;
+cIn.aBooth = 0.6;
 cIn.bBooth = 2;
 cIn.tetherLength = 50;
 cIn.meanElevationInRadians = 30*pi/180;
 
 % path parameter
 pathParam = linspace(0,2*pi,200);
-pathRange = [1 2];
+pathRange = [ 1 2];
 
 %% get equations
-[x,y,K] = cIn.circular_2D_Path();
+[x,y,z,K] = cIn.derive_3D_Equations();
 % find coordinates
 for ii = 1:numel(pathParam)
     R(ii) = min(1/max(eps,K(pathParam(ii))),cIn.tetherLength);
     xx(ii) = x(pathParam(ii));
     yy(ii) = y(pathParam(ii));
+    zz(ii) = z(pathParam(ii));
 end
 
 %% plots
 fig = cIn.findFigureObject('Results');
-set(gcf,'Position',fig.Position.*[1 0.1 1 2]);
+set(gcf,'Position',[100 0.1 560 2*420]);
 
 subplot(2,1,1);
 plot(pathParam,R,'k-','linewidth',1);
 grid on
 hold on
-xlabel('Path parameter'); ylabel('Radius of curvature (m)');
+xlabel('path parameter'); ylabel('R');
 
 subplot(2,1,2);
-plot(xx,yy,'k-','linewidth',1);
+plot3(xx,yy,zz,'k-','linewidth',1);
 hold on
 grid on
 axis equal
-xlabel('X (m)'); ylabel('Y (m)');
+xlabel('X (m)'); ylabel('Y (m)'); zlabel('Z (m)');
+cIn.plotDome;
+view(100,35);
+
 
 %% analyze section of plot
 nIdx = pathParam>=pathRange(1) &...
@@ -44,6 +49,6 @@ nIdx = pathParam>=pathRange(1) &...
 subplot(2,1,1)
 plot(pathParam(nIdx),R(nIdx),'r-','linewidth',1);
 subplot(2,1,2)
-plot(xx(nIdx),yy(nIdx),'r-','linewidth',1);
+plot3(xx(nIdx),yy(nIdx),zz(nIdx),'r-','linewidth',1);
 
 
