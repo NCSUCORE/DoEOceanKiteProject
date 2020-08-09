42cc0a3d65f3c6c4a0283e2d657a3d5c5f632dca
John <jborek@uncc.edu>
2020-08-09 19:33:36 -0400

    Created a new vehicle variant. Need to understand added mass before I claim it's correct - JB

--- vehicleDesign/AVL/functions/avlCreateInputFilePart_v2LE.m
+++ vehicleDesign/AVL/functions/avlCreateInputFilePart_v2LE.m
@@ -40,35 +40,11 @@ x_t_w = (b_w/2)*tand(sw_w);
 y_t_w = (b_w/2);
 z_t_w = (b_w/2)*tand(di_w);
 
-% spacing parameters
-N_b_w = w_Ns;
-N_c_w = w_Nc;
-
-% error messages
-if mod(N_c_w,1) ~= 0 || N_c_w <= 0
-    error('The calculated values of number of chordwise elements of wing will lead to errors in AVL')
-end
-if mod(N_b_w,1) ~= 0 || N_b_w <= 0
-    error('The calculated values of number of span elements of wing will lead to errors in AVL')
-end
-
 % horizonatal stbilizer calculations %%%%%%%%%%%%%
 % HS tip cordinates
 x_t_hs = (b_hs/2)*tand(sw_hs);
 y_t_hs = (b_hs/2);
 z_t_hs = (b_hs/2)*tand(di_hs);
-
-% spacing parameters
-N_b_hs = w_Ns;
-N_c_hs = w_Nc;
-
-% error messages
-if mod(N_c_hs,1) ~= 0 || N_c_hs <= 0
-    error('The calculated values of number of chordwise elements in HS will lead to errors in AVL')
-end
-if mod(N_b_hs,1) ~= 0 || N_b_hs <= 0
-    error('The calculated values of number of span elements in HS will lead to errors in AVL')
-end
 
 % vertical stabilizer calculations
 % HS tip cordinates
@@ -76,51 +52,367 @@ x_t_vs = b_vs*tand(sw_vs);
 y_t_vs = 0;
 z_t_vs = b_vs;
 
-% spacing parameters
-N_b_vs = w_Ns;
-N_c_vs = w_Nc;
-
-% error messages
-if mod(N_c_vs,1) ~= 0 || N_c_vs <= 0
-    error('The calculated values of number of chordwise elements in VS will lead to errors in AVL')
-end
-if mod(N_b_vs,1) ~= 0 || N_b_vs <= 0
-    error('The calculated values of number of span elements in VS will lead to errors in AVL')
-end
-
-
 %% AVL input file WING
 fileID = fopen(fullfile(fileparts(which('avl.exe')),'wing'),'w');
 
-% design name % Plane Vanilla
-% des_Name = 'Plane Vanilla test';
-fprintf(fileID,'%s\n','wing');
+% open file
+printName(fileID,'Wing');
 
 % Mach
 Mach = 0.0;
-fprintf(fileID,'#Mach\n');
-fprintf(fileID,'%0.2f\n',Mach);
+setMachNumber(fileID,Mach);
 
-% IYsym   IZsym   Zsym
-m_IYsym = 0;
-m_IZsym = 0;
-m_IXsym = 0;
-fprintf(fileID,'#IYsym   IZsym   Zsym\n');
-fprintf(fileID,'%d \t\t %d\t\t %d\n',m_IYsym,m_IZsym,m_IXsym);
+% IYsym, IZsym, Zsym
+setSymmetryProperties(fileID,0,0,0);
 
-%  Sref    Cref    Bref
+% Sref, Cref, Bref
 Sref =  S_w;
 Cref = (c_r_w + c_t_w)/2;
 Bref = b_w;
+
+setReferenceProperties(fileID,Sref,Cref,Bref,...
+    R_ref(1),R_ref(2),R_ref(3))
+
+sectionBreak(fileID);
+
+%% define wing: contains one section
+fprintf(fileID,'SURFACE\n');    % initialize surface
+fprintf(fileID,'Wing\n');       % initialize surface wing
+
+% Nchordwise, Cspace, Nspanwise, Sspace
+setAvlAnalysisParameters(fileID,w_Nc,1,w_Ns,1);
+
+% permanent incident angle
+setIncidenceAngle(fileID,w_inc_angle);
+sectionBreak(fileID);
+
+% SECTION 1
+fprintf(fileID,'SECTION\n'); 
+
+% Xle, Yle, Zle, Chord, Ainc, Nspanwise, Sspace
+defineSection(fileID,0,0,0,c_r_w,0,0,0);
+
+% airfoil name
+setAirfoil(fileID,w_afile)
+
+% Cname, Cgain, Xhinge, HingeVec, SgnDup
+Cgain_ail = 1;
+Xhinge_ail = 0.75;
+HingeVecAil = [0;0;0];
+SgnDup_ail = 1;
+claf_w = 1;
+
+% aileron
+defineCtrlSurface(fileID,'aileron',Cgain_ail,Xhinge_ail,...
+    HingeVecAil,SgnDup_ail);
+
+% CLAF
+setClaf(fileID,claf_w);
+sectionBreak(fileID);
+
+% SECTION 2
+fprintf(fileID,'SECTION\n'); 
+
+% Xle, Yle, Zle, Chord, Ainc, Nspanwise, Sspace
+defineSection(fileID,x_t_w,y_t_w,z_t_w,c_t_w,0,0,0);
+
+% airfoil name
+setAirfoil(fileID,w_afile)
+
+% aileron
+defineCtrlSurface(fileID,'aileron',Cgain_ail,Xhinge_ail,...
+    HingeVecAil,SgnDup_ail);
+
+% CLAF
+setClaf(fileID,claf_w);
+sectionBreak(fileID);
+
+fclose(fileID);
+
+%% AVL input file HS
+fileID = fopen(fullfile(fileparts(which('avl.exe')),'H_stab'),'w');
+
+% name
+printName(fileID,'H_stab');
+
+% Mach
+setMachNumber(fileID,Mach);
+
+% IYsym, IZsym, Zsym
+setSymmetryProperties(fileID,0,0,0);
+
+%  Sref, Cref, Bref, Xref, Yref, Zref
+setReferenceProperties(fileID,Sref,Cref,Bref,R_ref(1),R_ref(2),R_ref(3));
+sectionBreak(fileID);
+
+% initialize surface: H-stab
+fprintf(fileID,'SURFACE\n');    
+fprintf(fileID,'H-stab\n');    
+
+% Nchordwise, Cspace, Nspanwise, Sspace
+setAvlAnalysisParameters(fileID,w_Nc,1,w_Ns,1);
+
+% toggle Y-duplicate
+fprintf(fileID,'YDUPLICATE\n');
+fprintf(fileID,'%0.1f\n',0);
+
+% permanent incident angle
+setIncidenceAngle(fileID,hs_inc_angle);
+sectionBreak(fileID);
+
+% SECTION 1
+fprintf(fileID,'SECTION\n'); 
+
+% Xle, Yle, Zle, Chord, Ainc, Nspanwise, Sspace
+defineSection(fileID,0,0,0,c_r_hs,0,0,0);
+
+% horizontal stabilizer airfoil
+setAirfoil(fileID,hs_afile);
+
+% elevator (defined as aileron)
+defineCtrlSurface(fileID,'aileron',Cgain_ail,Xhinge_ail,...
+    HingeVecAil,SgnDup_ail);
+
+% CLAF
+setClaf(fileID,claf_w);
+sectionBreak(fileID);
+
+% SECTION 2
+fprintf(fileID,'SECTION\n');
+
+% Xle, Yle, Zle, Chord, Ainc, Nspanwise, Sspace
+defineSection(fileID,x_t_hs,y_t_hs,z_t_hs,c_t_hs,0,0,0);
+
+% horizontal stabilizer airfoil
+setAirfoil(fileID,hs_afile);
+
+% elevator (defined as aileron)
+defineCtrlSurface(fileID,'aileron',Cgain_ail,Xhinge_ail,...
+    HingeVecAil,SgnDup_ail);
+
+% CLAF
+setClaf(fileID,claf_w);
+sectionBreak(fileID);
+
+% close file
+fclose(fileID);
+
+
+%% AVL input file VS
+fileID = fopen(fullfile(fileparts(which('avl.exe')),'V_stab'),'w');
+
+% name
+printName(fileID,'V_stab');
+
+% Mach
+setMachNumber(fileID,Mach);
+
+% IYsym, IZsym, Zsym
+setSymmetryProperties(fileID,0,0,0);
+
+%  Sref, Cref, Bref, Xref, Yref, Zref
+setReferenceProperties(fileID,Sref,Cref,Bref,R_ref(1),R_ref(2),R_ref(3));
+sectionBreak(fileID);
+
+% define surface: V-stab
+fprintf(fileID,'SURFACE\n');    
+fprintf(fileID,'V-stab\n');       
+
+% Nchordwise, Cspace, Nspanwise, Sspace
+setAvlAnalysisParameters(fileID,w_Nc,1,w_Ns,1);
+
+% permanent incident angle
+setIncidenceAngle(fileID,w_inc_angle);
+sectionBreak(fileID);
+
+% SECTION 1
+fprintf(fileID,'SECTION\n');
+
+% Xle, Yle, Zle, Chord, Ainc, Nspanwise, Sspace
+defineSection(fileID,0,0,0,c_r_vs,0,0,0);
+
+% vertical stabilizer airfoil
+setAirfoil(fileID,vs_afile);
+
+% rudder (defined as aileron)
+defineCtrlSurface(fileID,'aileron',Cgain_ail,Xhinge_ail,...
+    HingeVecAil,SgnDup_ail);
+
+% CLAF
+setClaf(fileID,claf_w);
+sectionBreak(fileID);
+
+% SECTION 2
+fprintf(fileID,'SECTION\n'); % SECTION
+% Xle, Yle, Zle, Chord, Ainc, Nspanwise, Sspace
+defineSection(fileID,x_t_vs,z_t_vs,y_t_vs,c_t_vs,0,0,0);
+
+% vertical stabilizer airfoil
+setAirfoil(fileID,vs_afile);
+
+% rudder (defined as aileron)
+defineCtrlSurface(fileID,'aileron',Cgain_ail,Xhinge_ail,...
+    HingeVecAil,SgnDup_ail);
+
+% CLAF
+setClaf(fileID,claf_w);
+sectionBreak(fileID)
+
+fclose(fileID);
+
+end
+
+%% local functions
+
+% check the airfoil name, look for dat file and set in the input file
+function setAirfoil(fileID,airfoilName)
+
+if startsWith(airfoilName,'NACA')
+    fprintf(fileID,'\nNACA\n'); % AFILE
+    fprintf(fileID,'%s\n\n',strtrim(erase(airfoilName,'NACA'))); % filename
+elseif endsWith(airfoilName,'.dat')
+    if isfile(strcat(fileparts(which('avl.exe')),'\',airfoilName))
+        fprintf(fileID,'\nAFIL\n'); % AFILE
+        fprintf(fileID,'%s\n\n',airfoilName); % filename
+    else
+        error([airfoilName,' not found in ',fileparts(which('avl.exe'))]);
+    end
+else
+    error(['Please provide airfoil name property that either starts with ',...
+        '"NACA" for NACA airfoils or ends with ".dat" to load airfoil ',...
+        'from a dat file. eg. NACA0015 or afoil.dat']);
+end
+
+end
+
+function defineSection(fileID,xLeadingEdge,yLeadingEdge,zLeadingEdge,...
+    chord,incidenceAngle,spanwiseSections,sSpace)
+
+% Xle,Yle,Zle =  airfoil's leading edge location
+%   Chord       =  the airfoil's chord  (trailing edge is at Xle+Chord,Yle,Zle)
+%   Ainc        =  incidence angle, taken as a rotation (+ by RH rule) about 
+%                  the surface's spanwise axis projected onto the Y-Z plane.  
+%   Nspan       =  number of spanwise vortices until the next section 
+%   Sspace      =  controls the spanwise spacing of the vortices      
+  
+fprintf(fileID,'#Xle \t Yle \t Zle \t Chord \t Ainc \t Nspanwise \t Sspace\n');
+fprintf(fileID,'%0.2f \t %0.2f \t %0.2f \t %0.2f \t %0.2f \t %d \t\t\t %d\n',...
+    xLeadingEdge, yLeadingEdge, zLeadingEdge, chord, incidenceAngle,...
+    spanwiseSections,sSpace);
+end
+
+function setSymmetryProperties(fileID,iYsym,iZsym,Zsym)
+% iYsym =  1  case is symmetric about Y=0    , (X-Z plane is a solid wall)
+%       = -1  case is antisymmetric about Y=0, (X-Z plane is at const. Cp)
+%       =  0  no Y-symmetry is assumed
+% 
+% iZsym =  1  case is symmetric about Z=Zsym    , (X-Y plane is a solid wall)
+%       = -1  case is antisymmetric about Z=Zsym, (X-Y plane is at const. Cp)
+%       =  0  no Z-symmetry is assumed (Zsym ignored)
+
+fprintf(fileID,'#IYsym   IZsym   Zsym\n');
+fprintf(fileID,'%d \t\t %d\t\t %d\n',iYsym,iZsym,Zsym);
+end
+
+function setReferenceProperties(fileID,refArea,refChord,refSpan,...
+    xRef,yRef,zRef)
+
+% Sref  = reference area  used to define all coefficients (CL, CD, Cm, etc)
+% Cref  = reference chord used to define pitching moment (Cm)
+% Bref  = reference span  used to define roll,yaw moments (Cl,Cn)
+% 
+% X,Y,Zref = default location about which moments and rotation rates are defined
+%              (if doing trim calculations, XYZref must be the CG location,
+%               which can be imposed with the MSET command described later)
+
+% Sref, Cref, Bref
 fprintf(fileID,'#Sref    Cref    Bref\n');
-fprintf(fileID,'%0.2f \t %0.2f \t %0.2f\n',Sref,Cref,Bref);
-
-% Xref    Yref    Zref
-Xref = R_ref(1);
-Yref = R_ref(2);
-Zref = R_ref(3);
-
+fprintf(fileID,'%0.2f \t %0.2f \t %0.2f\n',refArea,refChord,refSpan);
+% Xref, Yref, Zref
 fprintf(fileID,'#Xref    Yref    Zref\n');
-fprintf(fileID,'%0.2f \t %0.2f \t %0.2f\n',Xref,Yref,Zref);
+fprintf(fileID,'%0.2f \t %0.2f \t %0.2f\n',xRef,yRef,zRef);
 
-fprintf(fileID,'#\n#\n#end
+end
+
+function defineCtrlSurface(fileID,typeOfSurface,ctrlGain,normalizedHingeLoc,...
+    hingeVec,signDuplication)
+% The CONTROL keyword declares that a hinge deflection at this section
+% is to be governed by one or more control variables.  An arbitrary number 
+% of control variables can be used, limited only by the array limit NDMAX.
+% 
+% The data line quantities are...
+% 
+%  name     name of control variable
+%  gain     control deflection gain, units:  degrees deflection / control variable
+%  Xhinge   x/c location of hinge.  
+%            If positive, control surface extent is Xhinge..1  (TE surface)
+%            If negative, control surface extent is 0..-Xhinge (LE surface)
+%  XYZhvec  vector giving hinge axis about which surface rotates 
+%            + deflection is + rotation about hinge vector by righthand rule
+%            Specifying XYZhvec = 0. 0. 0. puts the hinge vector along the hinge
+%  SgnDup   sign of deflection for duplicated surface
+%            An elevator would have SgnDup = +1
+%            An aileron  would have SgnDup = -1       
+
+fprintf(fileID,'CONTROL\n'); % CONTROL Section
+fprintf(fileID,'#Cname \t\tCgain \tx/c \txHinge \tyHinge \tzHinge \tSgnDup\n');
+fprintf(fileID,'%s \t%0.2f \t%0.2f \t%0.1f \t%0.1f \t%0.1f \t%0.1f\n\n',...
+    typeOfSurface,ctrlGain,normalizedHingeLoc,hingeVec(1),hingeVec(2),...
+    hingeVec(3),signDuplication);
+end
+
+function setClaf(fileID,claf)
+% This scales the effective dcl/da of the section airfoil as follows:
+%  dcl/da  =  2 pi CLaf
+% The implementation is simply a chordwise shift of the control point
+% relative to the bound vortex on each vortex element.
+% 
+% The intent is to better represent the lift characteristics 
+% of thick airfoils, which typically have greater dcl/da values
+% than thin airfoils.  A good estimate for CLaf from 2D potential
+% flow theory is
+% 
+%   CLaf  =  1 + 0.77 t/c
+% 
+% where t/c is the airfoil's thickness/chord ratio.  In practice,
+% viscous effects will reduce the 0.77 factor to something less.
+% Wind tunnel airfoil data or viscous airfoil calculations should
+% be consulted before choosing a suitable CLaf value.
+% 
+% If the CLAF keyword is absent for a section, CLaf defaults to 1.0, 
+% giving the usual thin-airfoil lift slope  dcl/da = 2 pi.  
+fprintf(fileID,'CLAF\n');
+fprintf(fileID,'%0.2f\n',claf);
+end
+
+function setAvlAnalysisParameters(fileID,nChord,cSpace,nSpan,sSpace)
+% Nchord =  number of chordwise horseshoe vortices placed on the surface
+% Cspace =  chordwise vortex spacing parameter (described later)
+% 
+% Nspan  =  number of spanwise horseshoe vortices placed on the surface [optional]
+% Sspace =  spanwise vortex spacing parameter (described later)         [optional]
+
+fprintf(fileID,'#Nchord \tCspace \tNspan \tSspace\n');
+fprintf(fileID,'%d \t\t\t%0.2f \t%d \t\t%0.2f\n',...
+    nChord,cSpace,nSpan,sSpace);
+
+end
+
+function setIncidenceAngle(fileID,incidenceAngle)
+fprintf(fileID,'\nANGLE\n');
+fprintf(fileID,'%0.1f\n',incidenceAngle);
+end
+
+function setMachNumber(fileID,machNumber)
+%   Mach  = default freestream Mach number for Prandtl-Glauert correction
+fprintf(fileID,'#Mach\n');
+fprintf(fileID,'%0.2f\n',machNumber);
+end
+
+function printName(fileID,name)
+fprintf(fileID,'%s\n',name);
+end
+
+function sectionBreak(fileID)
+fprintf(fileID,'#-------------------------------------------------------------\n');
+end
--- vehicleDesign/Geometric Optimization/Manta Ray/Stability_Analysis_new.m
+++ vehicleDesign/Geometric Optimization/Manta Ray/Stability_Analysis_new.m
@@ -1,51 +1,54 @@
 %%  Kite Design optimization
-clc;clear;
+% clc;clear;
 
 %%  Input definitions 
-loadComponent('Manta2RotNACA2412');                 %   Load vehicle 
-wing.alphaw = vhcl.portWing.alpha.Value;            %   Wing alpha vec
-wing.ARw = vhcl.portWing.AR.Value;                  %   Wing alpha vec
-wing.bw = 8;                                        %   Wing span
-wing.Sw = vhcl.fluidRefArea.Value;                  %   Reference area for wing
-wing.CLw = vhcl.portWing.CL.Value;                  %   Wing lift coefficient at zero alpha
-wing.CDw = vhcl.portWing.CD.Value;                  %   Wing drag coefficient at zero alpha
-wing.CDw_visc = 0.0297;                             %   Wing viscous drag coefficient
-wing.CDw_ind = 0.2697;                              %   Wing induced drag coefficient
+% loadComponent('Manta2RotNACA2412');                 %   Load vehicle 
+loadComponent('newManta2RotNACA2412');              %   Load vehicle 
+wing.alpha = vhcl.portWing.alpha.Value;             %   Wing alpha vec
+wing.AR = vhcl.portWing.AR.Value;                   %   Wing alpha vec
+wing.b = 8;                                         %   Wing span
+wing.S = vhcl.fluidRefArea.Value;                   %   Reference area for wing
+wing.CL = vhcl.portWing.CL.Value;                   %   Wing lift coefficient at zero alpha
+wing.CD = vhcl.portWing.CD.Value;                   %   Wing drag coefficient at zero alpha
+wing.CD_visc = 0.0297;                              %   Wing viscous drag coefficient
+wing.CD_ind = 0.2697;                               %   Wing induced drag coefficient
 wing.Cfe = 0.003;                                   %   Wing skin-friction drag coefficient
-wing.gammaw = 1;                                    %   Wing airfoil lift curve slope multiplicative constant
-wing.eLw = 0.9;                                     %   Wing lift Oswald efficiency factor
-wing.eDw = 0.9;                                     %   Wing drag Oswald efficiency factor
+wing.gamma = 1;                                     %   Wing airfoil lift curve slope multiplicative constant
+wing.eL = 0.9;                                      %   Wing lift Oswald efficiency factor
+wing.eD = 0.9;                                      %   Wing drag Oswald efficiency factor
 wing.aeroCent = [.1807 0 0]';                       %   Wing aerodynamic center 
 wing.E = 69e9;                                      %   Wing modulus of elasticity 
 
-hStab.alphah = vhcl.hStab.alpha.Value;              %   Horizontal stabilizer alpha vec
-hStab.CLh = vhcl.hStab.CL.Value;                    %   Horizontal stabilizer lift coefficient
-hStab.CDh = vhcl.hStab.CD.Value;                    %   Horizontal stabilizer drag coefficient
-hStab.ARh = vhcl.hStab.AR.Value;                    %   Horizontal stabilizer aspect ratio 
-hStab.Sh = vhcl.fluidRefArea.Value;                 %   Reference area for horizontal stabilizer
-hStab.gammah = 1;                                   %   Horizontal stabilizer airfoil lift curve slope multiplicative constant
-hStab.eLh = 0.9;                                    %   Horizontal stabilizer lift Oswald efficiency factor
-hStab.eDh = 0.9;                                    %   Horizontal stabilizer drag Oswald efficiency factor
+hStab.alpha = vhcl.hStab.alpha.Value;               %   Horizontal stabilizer alpha vec
+hStab.CL = vhcl.hStab.CL.Value;                     %   Horizontal stabilizer lift coefficient
+hStab.CD = vhcl.hStab.CD.Value;                     %   Horizontal stabilizer drag coefficient
+hStab.AR = vhcl.hStab.AR.Value;                     %   Horizontal stabilizer aspect ratio 
+hStab.S = vhcl.fluidRefArea.Value;                  %   Reference area for horizontal stabilizer
+hStab.gamma = 1;                                    %   Horizontal stabilizer airfoil lift curve slope multiplicative constant
+hStab.eL = 0.9;                                     %   Horizontal stabilizer lift Oswald efficiency factor
+hStab.eD = 0.9;                                     %   Horizontal stabilizer drag Oswald efficiency factor
 hStab.aeroCent = vhcl.hStab.rAeroCent_SurfLE.Value; %   Horizontal stabilizer aero center
+hstab.dCLElevator = 0.08;                           %   change in hstab CL per deg deflection of elevator
 
-vStab.alphav = vhcl.vStab.alpha.Value;              %   Find index corresponding to 0 AoA
-vStab.CDv = vhcl.vStab.CD.Value;                    %   Horizontal stabilizer drag coefficient at zero alpha
-vStab.Sv = vhcl.fluidRefArea.Value;                 %   Reference area for horizontal stabilizer
-vStab.eDv = 0.9;                                    %   Vertical stabilizer drag Oswald efficiency factor
+vStab.alpha = vhcl.vStab.alpha.Value;               %   Find index corresponding to 0 AoA
+vStab.CD = vhcl.vStab.CD.Value;                     %   Horizontal stabilizer drag coefficient at zero alpha
+vStab.S = vhcl.fluidRefArea.Value;                  %   Reference area for horizontal stabilizer
+vStab.eD = 0.9;                                     %   Vertical stabilizer drag Oswald efficiency factor
 vStab.aeroCent = [.1739 0 .9389]';                  %   Vertical stabilizer aero center
 
-fuse.CD0f = vhcl.fuse.endDragCoeff.Value;           %   Fuselage drag coefficient at 0° AoA
-fuse.CD9f = vhcl.fuse.sideDragCoeff.Value;          %   Fuselage drag coefficient at 90° AoA
+fuse.CD0 = vhcl.fuse.endDragCoeff.Value;            %   Fuselage drag coefficient at 0° AoA
+fuse.CDs = vhcl.fuse.sideDragCoeff.Value;           %   Fuselage drag coefficient at 90° AoA
 fuse.L = vhcl.fuse.length.Value;                    %   m - Length of fuselage 
+fuse.D = vhcl.fuse.diameter.Value;                  %   m - Fuselage diameter 
 
 Sys.m = vhcl.mass.Value;                            %   kg - vehicle mass
 Sys.B = 1;                                          %   Buoyancy factor
-Sys.xg = [1.6 0 0]';                                %   m - Center of gravity w/ respect to nose
-Sys.xb = Sys.xg+[.0171 0 .0546]';                   %   m - Center of buoyancy location 
-Sys.xbr = Sys.xg+[0 0 0]';                         %   m - Bridle location 
-Sys.xW = [1.6 0 0]'+wing.aeroCent;                  %   m - Wing aerodynamic center location 
-Sys.xH = [6 0 0]'+hStab.aeroCent;                   %   m - Horizontal stabilizer aerodynamic center location 
-Sys.xV = [5.88 0 0]'+vStab.aeroCent;                %   m - Vertical stabilizer aerodynamic center location 
+Sys.xg = vhcl.rCM_LE.Value-vhcl.fuse.rNose_LE.Value;%   m - Center of gravity w/ respect to nose
+Sys.xb = Sys.xg+[0 0 0]';                           %   m - Center of buoyancy location 
+Sys.xbr = Sys.xg+[0 0 0]';                          %   m - Bridle location 
+Sys.xW = -vhcl.fuse.rNose_LE.Value+wing.aeroCent;   %   m - Wing aerodynamic center location 
+Sys.xH = [5.95 0 0]'+hStab.aeroCent;                   %   m - Horizontal stabilizer aerodynamic center location 
+Sys.xV = [5.95 0 0]'+vStab.aeroCent;                %   m - Vertical stabilizer aerodynamic center location 
 
 Sys.vKite = [0 0 0]';                               %   m/s - Kite velocity 
 
@@ -66,17 +69,21 @@ Ang.heading = 0;                                        %   deg - Heading on the
 pitchMoment = zeros(3,numel(Ang.pitch));
 for i = 1:numel(Ang.pitch)
     Ang.tanPitch = Ang.pitch(i)-90+Ang.elevation;              %   deg - Tangent pitch angle
-    [M,F,CLh,CLhReq] = hStabCLcalc(Sys,Env,wing,hStab,vStab,fuse,Ang);
+    [M,F,CL,CD] = staticAnalysis(Sys,Env,wing,hStab,vStab,fuse,Ang);
     pitchMoment(:,i) = M.tot;
 end
-
+alphaRef = -10:.01:10;
+CLh = interp1(hStab.alpha,hStab.CL,alphaRef);
+idx = find(abs(CLh-CL.hReq) <= .0005);
+alphaRef(idx)
+CLh(idx)
 %%  Plotting 
 if numel(Ang.pitch) > 1
     figure; hold on; grid on
     plot(Ang.pitch,pitchMoment(2,:),'b-');  xlabel('$\theta$ [deg]');  ylabel('Pitch Moment')
 end
 % figure; hold on; grid on
-% plot(hStab.alphah-13.5,hStab.CLh,'b-');  xlabel('$\theta$ [deg]');  ylabel('CLh')
+% plot(hStab.alpha,hStab.CL,'b-');  xlabel('$\theta$ [deg]');  ylabel('CLh')
 
     
     
