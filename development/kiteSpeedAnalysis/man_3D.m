dbf5f9c4aca877b882a0a3d8708305347906747b
Ayaz <asiddiq2@ncsu.edu>
2020-08-11 00:34:10 -0400

    minor changes to test scripts and added a script to plot the lemniscate.

--- development/kiteSpeedAnalysis/man_3D.m
+++ development/kiteSpeedAnalysis/man_3D.m
@@ -6,7 +6,7 @@ clc
 cIn = maneuverabilityAnalysisLibrary;
 cIn.aBooth = 0.3491;
 cIn.bBooth = 0.6391;
-cIn.tetherLength = 400;
+cIn.tetherLength = 50;
 cIn.meanElevationInRadians = 30*pi/180;
 
 % path parameter
