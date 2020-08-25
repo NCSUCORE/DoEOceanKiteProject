6cdf6da34e8bf66c2f22127e18d06ebdf16617c2
John <jborek@uncc.edu>
2020-08-25 10:15:21 -0400

    Modified my static analysis script to use the structural design tools to verify that an arbitrary design is structurally feasible - JB

--- vehicleDesign/Geometric Optimization/Manta Ray/Optimization Functions/wingPowerCost2.m
+++ vehicleDesign/Geometric Optimization/Manta Ray/Optimization Functions/wingPowerCost2.m
@@ -1,26 +1,26 @@
 function [J,Flift] = wingPowerCost2(u,wing,hStab,vStab,fuse,Env)
 
 ARw = u(1); 
-alpha = u(2);
+alpha = u(2)*pi/180;
 
-Sw = wing.bw^2/ARw;
-Sh = hStab.Sh;
-Sv = vStab.Sv;
+Sw = wing.b^2/ARw;
+Sh = hStab.S;
+Sv = vStab.S;
 Sf = fuse.D^2*pi/4*cos(alpha)+(fuse.D^2*pi/4+fuse.D*fuse.L)*(1-cos(alpha));
 
-CLw =         interp1(wing.alphaw*pi/180,wing.CLw,alpha,'linear','extrap');
-CLh = (Sh/Sw)*interp1(hStab.alphah*pi/180,hStab.CLh,alpha,'linear','extrap');
+CLw =         interp1(wing.alpha*pi/180,wing.CL,alpha,'linear','extrap');
+CLh = (Sh/Sw)*interp1(hStab.alpha*pi/180,hStab.CL,alpha,'linear','extrap');
 
-CDw =         interp1(wing.alphaw*pi/180,wing.CDw,alpha,'linear','extrap');
-CDh = (Sh/Sw)*interp1(hStab.alphah*pi/180,hStab.CDh,alpha,'linear','extrap');
-CDv = (Sv/Sw)*interp1(vStab.alphav*pi/180,vStab.CDv,alpha,'linear','extrap');
-CDf = (Sf/Sw)*(fuse.CD0f*cos(alpha)+(1-cos(alpha)*fuse.CD9f));
+CDw =         interp1(wing.alpha*pi/180,wing.CD,alpha,'linear','extrap');
+CDh = (Sh/Sw)*interp1(hStab.alpha*pi/180,hStab.CD,alpha,'linear','extrap');
+CDv = (Sv/Sw)*interp1(vStab.alpha*pi/180,vStab.CD,alpha,'linear','extrap');
+CDf = (Sf/Sw)*(fuse.CD0*cos(alpha)+(1-cos(alpha)*fuse.CDs));
 
 CL = CLw + CLh;
 CD = CDw + CDh + CDv + CDf;
 
-Flift = 1/2*Env.rho*(CL/CD*Env.vFlow)^2*CL*Sw;
+Flift = 1/2*Env.rho*(CL/CD*Env.vFlow(1))^2*CL*Sw;
 
-J = -2/27*Env.rho*Env.vFlow^3*CL^3/CD^2*Sw;
+J = -2/27*Env.rho*Env.vFlow(1)^3*CL^3/CD^2*Sw;
 
 end 
