f9a2162edba16dfd98503143de363a0db39f5f67
jcreed2 <jcreed2@ncsu.edu>
2020-08-27 17:05:05 -0400

    Fixed the error that was decimating the DOE kite power JR

--- compositions/plants/components/vehicle/fluidDynamicSurface/fluidDynamicSurface_ts.m
+++ compositions/plants/components/vehicle/fluidDynamicSurface/fluidDynamicSurface_ts.m
@@ -1,21 +1,18 @@
 clear;clc
 
-loadComponent('ayazFullScaleOneThrVhcl')
+loadComponent('pathFollowingVhcl')
 
 fluidDensity = 1000;
-cSMax = 30;
-csMin = -30;
-velCMBdy   = [1 0 0];
+ctrlSurfDefl = [-30 30 0 0];
+velCMBdy   = [0 0 0];
 angVelBdy  = [0 0 0];
 
 data = [];
 for ii = 0.1:0.2:10
-velWindBdy = repmat([ii; 0; 0],1,4);
-ctrlSurfDefl = (csMin-cSMax).*rand(4,1) + cSMax;
+velWindBdy = [ii 0 0];
 sim('fluidDynamicSurface_th')
 data = [data;ii,sum(MBdy.Data,2)'];
 hold on
-keyboard
 end
 
 for ii = 1:3
