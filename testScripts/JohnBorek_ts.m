511fa68ca186224ddc87f4fe350910c0d6640c21
John <jborek@uncc.edu>
2020-08-27 18:08:25 -0400

    Double checked that everything works. Deleted Kartik's additional surface coefficient block. This should be implemented in the class def/build script (using different coefficient values). I will finish this tomorrow evening. I will also be creating a test script that anyone who is about to push, needs to run before pushing to verify they didn't break shit. Have a good night fam :) JB

--- testScripts/JohnBorek_ts.m
+++ testScripts/JohnBorek_ts.m
@@ -4,10 +4,10 @@ clear;clc;%close all
 %%  Select sim scenario
 %   0 = fig8;   1 = fig8-rotor;   1.1 = fig8-2rotor;   1.2 = fig8-2rotor New Model;
 %   2 = fig8-winch;   3 = steady;  4 = LaR;  4.2 = LaR New Model;
-simScenario = 4.2;
+simScenario = 1.2;
 %%  Set Physical Test Parameters
 thrLength = 400;                                            %   m - Initial tether length
-flwSpd = .25;                                               %   m/s - Flow speed
+flwSpd = .4;                                               %   m/s - Flow speed
 lengthScaleFactors = 0.8;                                   %   Factor to scale DOE kite to Manta Ray
 el = 30*pi/180;                                             %   rad - Mean elevation angle
 h = 10*pi/180;  w = 40*pi/180;                              %   rad - Path width/height
@@ -115,10 +115,10 @@ for ii = 1:numel(flwSpd)
     end
     tRef = [0  250 500 750 1000 1250 1500 1750 2000 2250 2500 2750 3000];
     pSP =  [30 30  30  30  30   40   40   40   40   40   40   40   40];
-    thr.tether1.dragEnable.setValue(0,'');
+    thr.tether1.dragEnable.setValue(1,'');
     % vhcl.rBridle_LE.setValue([0,0,0]','m');
     %%  Set up critical system parameters and run simulation
-    simParams = SIM.simParams;  simParams.setDuration(4000,'s');  dynamicCalc = '';
+    simParams = SIM.simParams;  simParams.setDuration(2000,'s');  dynamicCalc = '';
     simWithMonitor('OCTModel')
     %%  Log Results
     tsc = signalcontainer(logsout);
@@ -154,21 +154,21 @@ for ii = 1:numel(flwSpd)
     %     filename = sprintf(strcat('Pitch_kp-%.1f_ki-%.1f_',dt,'.mat'),fltCtrl.elevCmd.kp.Value,fltCtrl.elevCmd.ki.Value);
         fpath = fullfile(fileparts(which('OCTProject.prj')),'Results','Manta 2.0','LaR\');
     end
-    save(strcat(fpath,filename),'tsc','vhcl','thr','fltCtrl','env','simParams','LIBRARY','gndStn')
+%     save(strcat(fpath,filename),'tsc','vhcl','thr','fltCtrl','env','simParams','LIBRARY','gndStn')
 end
 %%  Animate Simulation
-if simScenario <= 2
-    vhcl.animateSim(tsc,2,'PathFunc',fltCtrl.fcnName.Value,...
-        'GifTimeStep',.05,'PlotTracer',true,'FontSize',12,'Pause',false,...
-        'ZoomIn',1==0,'SaveGif',1==1,'GifFile',strrep(filename,'.mat','.gif'));
-else
-    vhcl.animateSim(tsc,2,'View',[0,0],...
-        'GifTimeStep',.05,'PlotTracer',true,'FontSize',12,'ZoomIn',1==1,...
-        'SaveGif',1==1,'GifFile',strrep(filename,'.mat','zoom.gif'));
-end
+% if simScenario <= 2
+%     vhcl.animateSim(tsc,2,'PathFunc',fltCtrl.fcnName.Value,...
+%         'GifTimeStep',.05,'PlotTracer',true,'FontSize',12,'Pause',false,...
+%         'ZoomIn',1==0,'SaveGif',1==1,'GifFile',strrep(filename,'.mat','.gif'));
+% else
+%     vhcl.animateSim(tsc,2,'View',[0,0],...
+%         'GifTimeStep',.05,'PlotTracer',true,'FontSize',12,'ZoomIn',1==1,...
+%         'SaveGif',1==1,'GifFile',strrep(filename,'.mat','zoom.gif'));
+% end
 %%  Plot Results
 if simScenario < 3
-%     tsc.plotFlightResults(vhcl,env,'plot1Lap',1==1,'plotS',1==1,'Vapp',false,'plotBeta',1==1)
+    tsc.plotFlightResults(vhcl,env,'plot1Lap',1==1,'plotS',1==1,'Vapp',false,'plotBeta',1==1)
 %     tsc.plotTanAngles('plot1Lap',true,'plotS',true)
 %     tsc.plotPower(vhcl,env,'plot1Lap',true,'plotS',true,'Lap1',1,'Color',[0 0 1],'plotLoyd',false)
 else
