11ad6e141d35196e5b5ef1a0ff69e2072c94a265
adabney89 <adabney@ncsu.edu>
2020-10-05 10:04:36 -0400

    Updated exp scale kite - AA

--- testScripts/aaTS/andrewa_ts.m
+++ testScripts/aaTS/andrewa_ts.m
@@ -7,7 +7,7 @@ clear;clc;%close all
 %   2 = fig8-winch DOE;
 %   3 = steady Old;       3.1 = steady AVL;     3.2 = steady XFoil      3.3 = Steady XFlr5      3.4 = Steady XFlr5 Passive ;
 %   4 = LaR Old;          4.1 = LaR AVL;        4.2 = LaR XFoil;        4.3 = LaR XFlr5 
-simScenario = 1.3;
+simScenario = 3.5;
 simScenariosub = (simScenario - floor(simScenario))*10
 %%  Set Physical Test Parameters
 thrLength = 8;                                            %   m - Initial tether length
