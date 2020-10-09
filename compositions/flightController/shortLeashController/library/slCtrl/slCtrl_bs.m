b5a04a4d44966db0a220dd4c659d9a3178225aed
adabney89 <adabney@ncsu.edu>
2020-10-09 18:33:17 -0400

    Added new flight controller for short leash mode. AA Added plot scripts

---
+++ compositions/flightController/shortLeashController/library/slCtrl/slCtrl_bs.m
@@ -0,0 +1,84 @@
+FLIGHTCONTROLLER = 'slController';
+SPOOLINGCONTROLLER = 'universalSpoolingController';
+
+fltCtrl = CTR.SLFAndSpoolCtrl;
+
+fltCtrl.setSearchSize(.5,'');
+fltCtrl.setElevatorReelInDef(20,'deg')
+fltCtrl.firstSpoolLap.setValue(1,'');
+
+%%  Control parameters
+fltCtrl.tanRoll.kp.setValue(0.2,'(rad)/(rad)');
+fltCtrl.tanRoll.ki.setValue(0,'(rad)/(rad*s)');
+fltCtrl.tanRoll.kd.setValue(0,'(rad)/(rad/s)');
+fltCtrl.tanRoll.tau.setValue(1e-3,'s');
+
+fltCtrl.rollMoment.kp.setValue((10e4)/(11*pi/180),'(N*m)/(rad)')
+fltCtrl.rollMoment.ki.setValue(0,'(N*m)/(rad*s)');
+fltCtrl.rollMoment.kd.setValue((10e4)/(11*pi/180),'(N*m)/(rad/s)');
+fltCtrl.rollMoment.tau.setValue(0.001,'s');
+
+fltCtrl.yawMoment.kp.setValue((1e3)/(10*pi/180),'(N*m)/(rad)');
+
+fltCtrl.pitchSP.kp.setValue(10,'(deg)/(deg)');
+fltCtrl.pitchSP.ki.setValue(.01,'(deg)/(deg*s)');
+fltCtrl.pitchSP.kd.setValue(0,'(deg)/(deg/s)');
+fltCtrl.pitchSP.tau.setValue(.01,'s');
+fltCtrl.pitchSPkpSlope.setValue(.02368,'');
+fltCtrl.pitchSPkpInt.setValue(.5263,'');
+fltCtrl.pitchSPkiSlope.setValue(7.895e-5,'');
+fltCtrl.pitchSPkiInt.setValue(.008421,'');
+
+fltCtrl.yawSP.kp.setValue(10,'(deg)/(deg)');
+fltCtrl.yawSP.ki.setValue(.01,'(deg)/(deg*s)');
+fltCtrl.yawSP.kd.setValue(0,'(deg)/(deg/s)');
+fltCtrl.yawSP.tau.setValue(.01,'s');
+fltCtrl.yawSPkpSlope.setValue(.02368,'');
+fltCtrl.yawSPkpInt.setValue(.5263,'');
+fltCtrl.yawSPkiSlope.setValue(7.895e-5,'');
+fltCtrl.yawSPkiInt.setValue(.008421,'');
+
+fltCtrl.elevCmd.kp.setValue(200,'(deg)/(rad)');
+fltCtrl.elevCmd.ki.setValue(10,'(deg)/(rad*s)');
+fltCtrl.elevCmd.kd.setValue(0,'(deg)/(rad/s)');
+fltCtrl.elevCmd.tau.setValue(.01,'s');
+
+fltCtrl.rudderCmd.kp.setValue(200,'(deg)/(rad)');
+fltCtrl.rudderCmd.ki.setValue(0,'(deg)/(rad*s)');
+fltCtrl.rudderCmd.kd.setValue(0,'(deg)/(rad/s)');
+fltCtrl.rudderCmd.tau.setValue(.01,'s');
+
+fltCtrl.controlSigMax.upperLimit.setValue(30,'')
+fltCtrl.controlSigMax.lowerLimit.setValue(-30,'')
+
+fltCtrl.pitchAngleMax.upperLimit.setValue(45,'')
+fltCtrl.pitchAngleMax.lowerLimit.setValue(-45,'')
+
+fltCtrl.startControl.setValue(0,'s');
+
+fltCtrl.pitchCtrl.setValue(2,'');
+fltCtrl.pitchConst.setValue(0,'deg');
+fltCtrl.yawCtrl.setValue(1,'');
+fltCtrl.yawConst.setValue(0,'deg');
+fltCtrl.pitchTime.setValue([0 2000],'s');
+fltCtrl.pitchLookup.setValue([0 0],'deg');
+%% Spooling
+fltCtrl.setCtrlVecUpdateFcn('combinedCmd','')
+fltCtrl.setTetherLengthSetpointFcn('combinedTLSP','')
+fltCtrl.setWinchAndElevCmdFcn('combinedCmd','')
+fltCtrl.setInitSpdVec([0 0 0 0 0],'m/s')
+fltCtrl.setInitCtrlVec([.25 .14 0 0 0 0 0 0],'');
+fltCtrl.setIntraDrift(10,'m');
+fltCtrl.setInitTL(80,'m')
+fltCtrl.setMaxTL(400,'m')
+fltCtrl.setSwitchFilterConstant(.1,'s')
+fltCtrl.setSwitchFilterDuration(10,'s')
+fltCtrl.setNonXCurrentSpoolInGain(1.5,'')
+fltCtrl.setSpoolCtrlTimeConstant(5,'s')
+fltCtrl.setNomSpoolSpeed(0,'m/s')
+fltCtrl.setShortLeashLength(20,'m')
+fltCtrl.LaRelevationSP.setValue(45,'deg');
+fltCtrl.LaRelevationSPErr.setValue(1,'deg');
+%% Save
+saveFile = saveBuildFile('fltCtrl',mfilename,'variant','FLIGHTCONTROLLER');
+save(saveFile,'SPOOLINGCONTROLLER','-append')
--- testScripts/aaTS/plotEig.m
+++ testScripts/aaTS/plotEig.m
@@ -2,42 +2,31 @@ clc
 clear all
 close all
 
-addpath('C:\Users\andre\Documents\Data\linSys')
+%addpath('C:\Users\andre\Documents\Data\linSys\Manta_AR8_B8\')
 load('linDyn')
-flwSpdArray = [0.25:.25:4];
-AoA = [0:2:16];
-thrLenArray = [10:5:50];
+flwSpdArray = 2%[0.25:.25:2];
+AoA = [0:1:10];
+thrLenArray = 20%[20:5:50];
 ilen = length(thrLenArray);
 jlen = length(flwSpdArray);
 klen = length(AoA);
 
 %Calculate time delay due to pade approximation
-delay = 6.47./flwSpdArray;
+delay = 6.75./flwSpdArray;
 
 %% Loop over Flow Speed
-for i = 1:ilen
-for k = 1:klen
-    moviename = sprintf('%d_%d_Lat',5+i*5,k*2-2);
-    f1 = figure%('visible','off');    
-    x1 = 0;
-    x2 = 0;
-    y1 = 0;
-    for j = 1:jlen
-        x1t = floor(min(real(pole(linDyn.Lat{i,j,k}))));
-        x2t = ceil(max(real(pole(linDyn.Lat{i,j,k}))));
-        y1t = floor(min(imag(pole(linDyn.Lat{i,j,k}))));
-        if x1t < x1
-            x1 = x1t;
-        end
-        if x2t > x2
-            x2 = x2t;
-        end
-        if y1t < y1
-            y1 = y1t;
-        end
-%         x1t = floor(min(real(zero(linDyn.Long{i,j,k}))));
-%         x2t = ceil(max(real(zero(linDyn.Long{i,j,k}))));
-%         y1t = floor(min(imag(zero(linDyn.Long{i,j,k}))));
+% for i = 1:ilen
+% for k = 1:klen
+%     moviename = sprintf('%d_m%d_degLatWide',thrLenArray(i),AoA(k));
+%     moviename2 = sprintf('%d_m%d_degLat',thrLenArray(i),AoA(k));
+%     f1 = figure%('visible','off');    
+%     x1 = 0;
+%     x2 = 0;
+%     y1 = 0;
+%     for j = 1:jlen
+%         x1t = floor(min(real(pole(linDyn.Lat{i,j,k}))));
+%         x2t = ceil(max(real(pole(linDyn.Lat{i,j,k}))));
+%         y1t = floor(min(imag(pole(linDyn.Lat{i,j,k}))));
 %         if x1t < x1
 %             x1 = x1t;
 %         end
@@ -47,99 +36,125 @@ for k = 1:klen
 %         if y1t < y1
 %             y1 = y1t;
 %         end
-    end
-    for j = 1:jlen
-        pzmap(linDyn.Lat{i,j,k},'b')
-        hold on
-        [num,den]=pade(delay(j),1);
-        plot(num(2),0,'ro')
-        plot(-den(2),0,'rx')
-        hold off
-        title({'Lateral Dynamics Pole Map w/ Pade Delay Approximation',...
-            sprintf('Tether Length = %d m, AoA = %d degrees, Flow Speed = %.2f m/s'...
-            ,thrLenArray(i),AoA(k),flwSpdArray(j))})
-        xlim([x1 2])
-        ylim([y1 -y1])
-        f = getframe(f1);
-        im = frame2im(f);
-        [imind,cm]  = rgb2ind(im,256);
-        if j == 1
-            imwrite(imind,cm,moviename,'gif', 'Loopcount',inf,'DelayTime',1/3);
-        else
-            imwrite(imind,cm,moviename,'gif','WriteMode','append','DelayTime',1/3)
-        end
-    end
-close all
-    
-    for j = 1:jlen
-        x1t = floor(min(real(pole(linDyn.Long{i,j,k}))));
-        x2t = ceil(max(real(pole(linDyn.Long{i,j,k}))));
-        y1t = floor(min(imag(pole(linDyn.Long{i,j,k}))));
-        if x1t < x1
-            x1 = x1t;
-        end
-        if x2t > x2
-            x2 = x2t;
-        end
-        if y1t < y1
-            y1 = y1t;
-        end
-    end
- % Make movie of longitudinal dynamics   
-        moviename = sprintf('%d_%d_Long',5+i*5,k*2-2);
-        f1 = figure
-    for j = 1:jlen
-        pzmap(linDyn.Long{i,j,k},'b')
-        hold on
-        [num,den]=pade(delay(j),1);
-        plot(num(2),0,'ro')
-        plot(-den(2),0,'rx')
-        hold off
-        title({'Longitudinal Dynamics Pole Map w/ Pade Delay Approximation',...
-            sprintf('Tether Length = %d m, AoA = %d degrees, Flow Speed = %.2f m/s'...
-            ,thrLenArray(i),AoA(k),flwSpdArray(j))})
-        hold off
-        xlim([x1 2])
-        ylim([y1 -y1])
-        f = getframe(f1);
-        im = frame2im(f);
-        [imind,cm]  = rgb2ind(im,256);
-        if j == 1
-            imwrite(imind,cm,moviename,'gif', 'Loopcount',inf,'DelayTime',1/3);
-        else
-            imwrite(imind,cm,moviename,'gif','WriteMode','append','DelayTime',1/3)
-        end
-    end
-    close all
-end
-end
+% %         x1t = floor(min(real(zero(linDyn.Long{i,j,k}))));
+% %         x2t = ceil(max(real(zero(linDyn.Long{i,j,k}))));
+% %         y1t = floor(min(imag(zero(linDyn.Long{i,j,k}))));
+% %         if x1t < x1
+% %             x1 = x1t;
+% %         end
+% %         if x2t > x2
+% %             x2 = x2t;
+% %         end
+% %         if y1t < y1
+% %             y1 = y1t;
+% %         end
+%     end
+%     for j = 1:jlen
+%         pzmap(linDyn.Lat{i,j,k},'b')
+%         hold on
+%         [num,den]=pade(delay(j),1);
+%         plot(num(2),0,'ro')
+%         plot(-den(2),0,'rx')
+%         hold off
+%         title({'Lateral Dynamics Pole Map w/ Pade Delay Approximation',...
+%             sprintf('Tether Length = %d m, AoA = %d degrees, Flow Speed = %.2f m/s'...
+%             ,thrLenArray(i),AoA(k),flwSpdArray(j))})
+%         xlim([x1 2])
+%         ylim([y1 -y1])        
+% %         xlim([-.25 0.25])
+% %         ylim([y1 -y1])
+%         f = getframe(f1);
+%         im = frame2im(f);
+%         [imind,cm]  = rgb2ind(im,256);
+%         if j == 1
+%             imwrite(imind,cm,moviename,'gif', 'Loopcount',inf,'DelayTime',1/3);
+%         else
+%             imwrite(imind,cm,moviename,'gif','WriteMode','append','DelayTime',1/3)
+%         end
+%         xlim([-.1 0.1])
+%         ylim([y1 -y1])
+%         f = getframe(f1);
+%         im = frame2im(f);
+%         [imind,cm]  = rgb2ind(im,256);
+%         if j == 1
+%             imwrite(imind,cm,moviename2,'gif', 'Loopcount',inf,'DelayTime',1/3);
+%         else
+%             imwrite(imind,cm,moviename2,'gif','WriteMode','append','DelayTime',1/3)
+%         end
+%     end
+% close all
+%     
+%     for j = 1:jlen
+%         x1t = floor(min(real(pole(linDyn.Long{i,j,k}))));
+%         x2t = ceil(max(real(pole(linDyn.Long{i,j,k}))));
+%         y1t = floor(min(imag(pole(linDyn.Long{i,j,k}))));
+%         if x1t < x1
+%             x1 = x1t;
+%         end
+%         if x2t > x2
+%             x2 = x2t;
+%         end
+%         if y1t < y1
+%             y1 = y1t;
+%         end
+%     end
+%  % Make movie of longitudinal dynamics   
+%         moviename = sprintf('%d_m%d_degLongwide',thrLenArray(i),AoA(k));
+%         moviename2 = sprintf('%d_m%d_degLong',thrLenArray(i),AoA(k));
+%         f1 = figure
+%     for j = 1:jlen
+%         pzmap(linDyn.Long{i,j,k},'b')
+%         hold on
+%         [num,den]=pade(delay(j),1);
+%         plot(num(2),0,'ro')
+%         plot(-den(2),0,'rx')
+%         hold off
+%         title({'Longitudinal Dynamics Pole Map w/ Pade Delay Approximation',...
+%             sprintf('Tether Length = %d m, AoA = %d degrees, Flow Speed = %.2f m/s'...
+%             ,thrLenArray(i),AoA(k),flwSpdArray(j))})
+%         hold off
+%         xlim([x1 2])
+%         ylim([y1 -y1])
+% %         xlim([-.25 0.25])
+% %         ylim([y1 -y1])
+%         f = getframe(f1);
+%         im = frame2im(f);
+%         [imind,cm]  = rgb2ind(im,256);
+%         if j == 1
+%             imwrite(imind,cm,moviename,'gif', 'Loopcount',inf,'DelayTime',1/3);
+%         else
+%             imwrite(imind,cm,moviename,'gif','WriteMode','append','DelayTime',1/3)
+%         end
+% 
+%         xlim([-.1 0.1])
+%         ylim([y1 -y1])
+%         f = getframe(f1);
+%         im = frame2im(f);
+%         [imind,cm]  = rgb2ind(im,256);
+%         if j == 1
+%             imwrite(imind,cm,moviename2,'gif', 'Loopcount',inf,'DelayTime',1/3);
+%         else
+%             imwrite(imind,cm,moviename2,'gif','WriteMode','append','DelayTime',1/3)
+%         end
+%     end
+%     close all
+% end
+% end
 
 %% Loop over Tether Length
-for j = 1:jlen
-for k = 1:klen
-    moviename = sprintf('%dms_%ddeg_Lat',25*j,k*2-2);
-    [num,den]=pade(delay(j),1);
-    f1 = figure%('visible','off');    
-    x1 = 0;
-    x2 = den(2);
-    y1 = 0;
-    
-    for i = 1%:ilen
-        x1t = floor(min(real(pole(linDyn.Lat{i,j,k}))));
-        x2t = ceil(max(real(pole(linDyn.Lat{i,j,k}))));
-        y1t = floor(min(imag(pole(linDyn.Lat{i,j,k}))));
-        if x1t < x1
-            x1 = x1t;
-        end
-        if x2t > x2
-            x2 = x2t;
-        end
-        if y1t < y1
-            y1 = y1t;
-        end
-%         x1t = floor(min(real(zero(linDyn.Long{i,j,k}))));
-%         x2t = ceil(max(real(zero(linDyn.Long{i,j,k}))));
-%         y1t = floor(min(imag(zero(linDyn.Long{i,j,k}))));
+% for j = 1:jlen
+% for k = 1:klen
+%     moviename = sprintf('%dms_%ddeg_Lat',flwSpdArray(j)*100,AoA(k));
+%     [num,den]=pade(delay(j),1);
+%     f1 = figure%('visible','off');    
+%     x1 = 0;
+%     x2 = den(2);
+%     y1 = 0;
+%     
+%     for i = 1%:ilen
+%         x1t = floor(min(real(pole(linDyn.Lat{i,j,k}))));
+%         x2t = ceil(max(real(pole(linDyn.Lat{i,j,k}))));
+%         y1t = floor(min(imag(pole(linDyn.Lat{i,j,k}))));
 %         if x1t < x1
 %             x1 = x1t;
 %         end
@@ -149,79 +164,91 @@ for k = 1:klen
 %         if y1t < y1
 %             y1 = y1t;
 %         end
-    end
-    for i = 1:ilen
-        pzmap(linDyn.Lat{i,j,k},'b')
-        hold on
-        [num,den]=pade(delay(j),1);
-        plot(num(2),0,'ro')
-        plot(-den(2),0,'rx')
-        hold off
-        xlim([x1 1])
-        ylim([y1 -y1])
-        title({'Lateral Dynamics Pole Map w/ Pade Delay Approximation',...
-            sprintf('Tether Length = %d m, AoA = %d degrees, Flow Speed = %.2f m/s'...
-            ,thrLenArray(i),AoA(k),flwSpdArray(j))})
-        f = getframe(f1);
-        im = frame2im(f);
-        [imind,cm]  = rgb2ind(im,256);
-        if i == 1
-            imwrite(imind,cm,moviename,'gif', 'Loopcount',inf,'DelayTime',1/3);
-        else
-            imwrite(imind,cm,moviename,'gif','WriteMode','append','DelayTime',1/3)
-        end
-    end
-close all
-    x1 = 0;
-    x2 = den(2);
-    y1 = 0;
-    for i = 1:ilen
-        x1t = floor(min(real(pole(linDyn.Long{i,j,k}))));
-        x2t = ceil(max(real(pole(linDyn.Long{i,j,k}))));
-        y1t = floor(min(imag(pole(linDyn.Long{i,j,k}))));
-        if x1t < x1
-            x1 = x1t;
-        end
-        if x2t > x2
-            x2 = x2t;
-        end
-        if y1t < y1
-            y1 = y1t;
-        end
-    end
- % Make movie of longitudinal dynamics   
-        moviename = sprintf('%dms_%ddeg_Long',25*j,k*2-2);
-        f1 = figure
-    for i = 1:ilen
-        pzmap(linDyn.Long{i,j,k},'b')
-        hold on
-
-        plot(num(2),0,'ro')
-        plot(-den(2),0,'rx')
-        hold off
-        title({'Longitudinal Dynamics Pole Map w/ Pade Delay Approximation',...
-            sprintf('Tether Length = %d m, AoA = %d degrees, Flow Speed = %.2f m/s'...
-            ,thrLenArray(i),AoA(k),flwSpdArray(j))})
-        hold off
-        xlim([x1 1])
-        ylim([y1 -y1])
-        f = getframe(f1);
-        im = frame2im(f);
-        [imind,cm]  = rgb2ind(im,256);
-        if i == 1
-            imwrite(imind,cm,moviename,'gif', 'Loopcount',inf,'DelayTime',1/3);
-        else
-            imwrite(imind,cm,moviename,'gif','WriteMode','append','DelayTime',1/3)
-        end
-    end
-    close all
-end
-end
-
-%% Loop over Pitch Angle
+% %         x1t = floor(min(real(zero(linDyn.Long{i,j,k}))));
+% %         x2t = ceil(max(real(zero(linDyn.Long{i,j,k}))));
+% %         y1t = floor(min(imag(zero(linDyn.Long{i,j,k}))));
+% %         if x1t < x1
+% %             x1 = x1t;
+% %         end
+% %         if x2t > x2
+% %             x2 = x2t;
+% %         end
+% %         if y1t < y1
+% %             y1 = y1t;
+% %         end
+%     end
+%     for i = 1:ilen
+%         pzmap(linDyn.Lat{i,j,k},'b')
+%         hold on
+%         [num,den]=pade(delay(j),1);
+%         plot(num(2),0,'ro')
+%         plot(-den(2),0,'rx')
+%         hold off
+%         xlim([x1 1])
+%         ylim([y1 -y1])
+%         title({'Lateral Dynamics Pole Map w/ Pade Delay Approximation',...
+%             sprintf('Tether Length = %d m, AoA = %d degrees, Flow Speed = %.2f m/s'...
+%             ,thrLenArray(i),AoA(k),flwSpdArray(j))})
+%         f = getframe(f1);
+%         im = frame2im(f);
+%         [imind,cm]  = rgb2ind(im,256);
+%         if i == 1
+%             imwrite(imind,cm,moviename,'gif', 'Loopcount',inf,'DelayTime',1/3);
+%         else
+%             imwrite(imind,cm,moviename,'gif','WriteMode','append','DelayTime',1/3)
+%         end
+%     end
+% close all
+%     x1 = 0;
+%     x2 = den(2);
+%     y1 = 0;
+%     for i = 1:ilen
+%         x1t = floor(min(real(pole(linDyn.Long{i,j,k}))));
+%         x2t = ceil(max(real(pole(linDyn.Long{i,j,k}))));
+%         y1t = floor(min(imag(pole(linDyn.Long{i,j,k}))));
+%         if x1t < x1
+%             x1 = x1t;
+%         end
+%         if x2t > x2
+%             x2 = x2t;
+%         end
+%         if y1t < y1
+%             y1 = y1t;
+%         end
+%     end
+%  % Make movie of longitudinal dynamics   
+%         moviename = sprintf('%dms_%ddeg_Long',flwSpdArray(j)*100,AoA(k));
+%         f1 = figure
+%     for i = 1:ilen
+%         pzmap(linDyn.Long{i,j,k},'b')
+%         hold on
+% 
+%         plot(num(2),0,'ro')
+%         plot(-den(2),0,'rx')
+%         hold off
+%         title({'Longitudinal Dynamics Pole Map w/ Pade Delay Approximation',...
+%             sprintf('Tether Length = %d m, AoA = %d degrees, Flow Speed = %.2f m/s'...
+%             ,thrLenArray(i),AoA(k),flwSpdArray(j))})
+%         hold off
+%         xlim([x1 1])
+%         ylim([y1 -y1])
+%         f = getframe(f1);
+%         im = frame2im(f);
+%         [imind,cm]  = rgb2ind(im,256);
+%         if i == 1
+%             imwrite(imind,cm,moviename,'gif', 'Loopcount',inf,'DelayTime',1/3);
+%         else
+%             imwrite(imind,cm,moviename,'gif','WriteMode','append','DelayTime',1/3)
+%         end
+%     end
+%     close all
+% end
+% end
+% 
+% %% Loop over Pitch Angle
 for j = 1:jlen
 for i = 1:ilen
-    moviename = sprintf('%dm_%dms_Lat',5+5*i,25*j);
+    moviename = sprintf('%dm_%dms_Lat',thrLenArray(i),flwSpdArray(j)*100);
     f1 = figure%('visible','off');    
     x1 = 0;
     x2 = 0;
@@ -290,7 +317,7 @@ close all
         end
     end
  % Make movie of longitudinal dynamics   
-        moviename = sprintf('%dm_%dms_Long',5+5*i,25*j);
+        moviename = sprintf('%dm_%dms_Long',thrLenArray(i),flwSpdArray(j)*100);
         f1 = figure
     for k = 1:klen
         pzmap(linDyn.Long{i,j,k},'b')
