b2b3e5a0a535a886608d69f5004e26256a1054c3
Ayaz <asiddiq2@ncsu.edu>
2020-10-12 15:22:09 -0400

    added environment for airborne testing.

---
+++ compositions/environment/flowCalculation/constXYZT/ayazAirborneFlow/ayazAirborneFlow_bs.m
@@ -0,0 +1,28 @@
+clear all;clc;format compact
+
+%% Set up environment
+% Create
+env = ENV.env;
+env.addFlow({'air'},{'constXYZT'},'FlowDensities',1.225);
+% 
+% env.waterWave.wave1.waveNumber.setValue(1,'rad/m')
+% env.waterWave.wave1.frequency.setValue(.2,'rad/s')
+% env.waterWave.wave1.amplitude.setValue(.1,'m')
+% env.waterWave.wave1.phase.setValue(0,'rad')
+% 
+% env.waterWave.wave2.waveNumber.setValue(1,'rad/m')
+% env.waterWave.wave2.frequency.setValue(.4,'rad/s')
+% env.waterWave.wave2.amplitude.setValue(.1,'m')
+% env.waterWave.wave2.phase.setValue(0,'rad')
+% 
+% env.waterWave.wave3.waveNumber.setValue(1,'rad/m')
+% env.waterWave.wave3.frequency.setValue(.6,'rad/s')
+% env.waterWave.wave3.amplitude.setValue(.1,'m')
+% env.waterWave.wave3.phase.setValue(0,'rad')
+
+% env.waterWave.waveParamMat.setValue(env.waterWave.structAssem,'');
+% FLOWCALCULATION = 'constXYZT_planarWave';
+FLOWCALCULATION = 'constXYZT';
+ENVIRONMENT     = 'environmentDOE';
+
+saveBuildFile('env',mfilename,'variant',["FLOWCALCULATION","ENVIRONMENT"]);
