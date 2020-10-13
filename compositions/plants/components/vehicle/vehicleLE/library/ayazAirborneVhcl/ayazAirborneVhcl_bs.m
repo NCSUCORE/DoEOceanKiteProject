606d57f4cde1083427c9bbff0b5f594622597893
Ayaz <asiddiq2@ncsu.edu>
2020-10-13 00:39:53 -0400

    added controller build scripts for airborne system simulations.

--- compositions/plants/components/vehicle/vehicleLE/library/ayazAirborneVhcl/ayazAirborneVhcl_bs.m
+++ compositions/plants/components/vehicle/vehicleLE/library/ayazAirborneVhcl/ayazAirborneVhcl_bs.m
@@ -14,16 +14,16 @@ loadComponent('ayazAirborneFlow');
 vhcl.setFluidDensity(env.air.density.Value,'kg/m^3')
 vhcl.setNumTethers(1,'');
 vhcl.setBuoyFactor(1.0,'');
-vhcl.setFluidCoeffsFileName('ayazFullScaleOneThrFluidCoeffs_v2','');
+vhcl.setFluidCoeffsFileName('ayazAirborneCoeff_v1','');
 
 %% Volumes and Inertia
 vhcl.setVolume(2.85698,'m^3') %From CAD
-Ixx=1.094057613168724e+04;
-Iyy=1.247604938271605e+04;
-Izz=2.467555555555556e+04;
-Ixy=0;
-Ixz=5.293827160493829e+02;
-Iyz=0;
+Ixx = 25;
+Iyy = 32;
+Izz = 56;
+Ixy = 0;
+Ixz = 0.47;
+Iyz = 0;
 
 vhcl.setInertia_CM([Ixx -Ixy -Ixz;...
                     -Ixy Iyy -Iyz;...
@@ -33,15 +33,10 @@ vhcl.setInertia_CM([Ixx -Ixy -Ixz;...
 vhcl.setRB_LE([0 0 0],'m');
 vhcl.setRCM_LE([0 0 0],'m');
 vhcl.setRBridle_LE(vhcl.rCM_LE.Value + [0;0;0],'m');
-vhcl.setRCentOfBuoy_LE([0.02136;0;0.0682],'m');% [.492,0,.0682] from CAD
+vhcl.setRCentOfBuoy_LE([0;0;0],'m');% [.492,0,.0682] from CAD
 
 %% Added Mass/Damping (defaults to zeros)
-vhcl.setMa6x6_LE(-0*[134         0           0           0          14           0;...
-                   0      1591           0        -986           0        5166;...
-                   0         0        9265           0       -8202           0;...
-                   0      -986           0       67412           0       -4608;...
-                  14         0       -8202           0       20209           0;...
-                   0      5166           0       -4608           0       23320;],'');
+vhcl.setMa6x6_LE(zeros(6),'');
 % vhcl.setD6x6_B([],'');
 
 %% Control Surfaces
@@ -53,10 +48,10 @@ vhcl.setAllMaxCtrlDefSpeed(60,'deg/s');
 clMax = 5;
 
 %% Wing
-vhcl.setWingRootChord(1,'m');
-vhcl.setWingAR(10,''); %Span 10, hspan 5
-vhcl.setWingTR(0.8,'');
-vhcl.setWingSweep(2.3,'deg');
+vhcl.setWingRootChord(0.666,'m');
+vhcl.setWingAR(8.25,''); %Span 10, hspan 5
+vhcl.setWingTR(2/3,'');
+vhcl.setWingSweep(0,'deg');
 vhcl.setWingDihedral(2,'deg');
 vhcl.setWingIncidence(0,'deg');
 vhcl.setWingAirfoil('NACA2412','');
@@ -64,32 +59,33 @@ vhcl.setWingClMin(-clMax,'');
 vhcl.setWingClMax(clMax,'');
 
 %% H-stab and V-stab
-vhcl.hStab.setRSurfLE_WingLEBdy([5.5;0;0],'m');
+vhcl.hStab.setRSurfLE_WingLEBdy([2;0;0],'m');
 vhcl.hStab.setNumTraps(2,'');
-vhcl.hStab.setRootChord(.5,'m');
-vhcl.hStab.setTR(.8,'');
-vhcl.hStab.setHalfSpanGivenAR(4/.45,''); %Span 4, hspan 2
-vhcl.hStab.setSweep(2.8624,'deg');
-vhcl.hStab.setIncidence(-13.5,'deg');
+vhcl.hStab.setRootChord(.29,'m');
+vhcl.hStab.setTR(1,'');
+vhcl.hStab.setHalfSpanGivenAR(4.65,''); %Span 4, hspan 2
+vhcl.hStab.setSweep(0,'deg');
+vhcl.hStab.setIncidence(0,'deg');
 vhcl.hStab.setAirfoil('NACA0015','');
 vhcl.hStab.setClMin(-clMax,'');
 vhcl.hStab.setClMax(clMax,'');
 
-vhcl.vStab.setRSurfLE_WingLEBdy([5.35;0;0],'m');
-vhcl.vStab.setRootChord(.65,'m');
-vhcl.vStab.setHalfSpan(2.4375,'m');
-vhcl.vStab.setTR(.8,'');
-vhcl.vStab.setSweep(3.44,'deg');
+vhcl.vStab.setRSurfLE_WingLEBdy([1.892;0;0],'m');
+vhcl.vStab.setRootChord(.398,'m');
+vhcl.vStab.setHalfSpan(.609,'m');
+vhcl.vStab.setTR(.9,'');
+vhcl.vStab.setSweep(3,'deg');
 vhcl.vStab.setAirfoil('NACA0015','');
 vhcl.vStab.setClMin(-clMax,'');
 vhcl.vStab.setClMax(clMax,'');
 
 %% Fuselage (could use more realistic numbers)
-vhcl.fuse.setDiameter(0.4445,'m');
+vhcl.fuse.setDiameter(0.08,'m');
 vhcl.fuse.setEndDragCoeff(0,'');
 vhcl.fuse.setSideDragCoeff(0,'');
-vhcl.fuse.setRNose_LE([-2;0;0],'m');
-vhcl.fuse.setREnd_LE([max(vhcl.hStab.rSurfLE_WingLEBdy.Value(1),vhcl.vStab.rSurfLE_WingLEBdy.Value(1));0;0],'m');
+vhcl.fuse.setRNose_LE([-1;0;0],'m');
+vhcl.fuse.setREnd_LE([max(vhcl.hStab.rSurfLE_WingLEBdy.Value(1)+vhcl.hStab.rootChord.Value,...
+    vhcl.vStab.rSurfLE_WingLEBdy.Value(1)+vhcl.vStab.rootChord.Value);0;0],'m');
 
 %% Turbines
 vhcl.setNumTurbines(2,'');
