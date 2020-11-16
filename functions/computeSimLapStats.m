ab44ac87db4efbb03dce39b906f38ffc3c1b11ac
Ayaz <asiddiq2@ncsu.edu>
2020-11-16 11:38:55 -0500

    wrote script to run airborne system simulations in loop.

--- functions/computeSimLapStats.m
+++ functions/computeSimLapStats.m
@@ -1,54 +1,56 @@
 function val = computeSimLapStats(obj)
 
-pathParam = squeeze(obj.currentPathVar.Data);
 lapsStarted = unique(obj.lapNumS.Data);
 plotLap = max(lapsStarted)-1;
-%  Determine Single Lap Indices
+%  Determine last lap Indices
 lapNum = squeeze(obj.lapNumS.Data);
 Idx1 = find(lapNum == plotLap,1,'first');
 Idx2 = find(lapNum == plotLap,1,'last');
 ran = Idx1:Idx2;
-% v_Appx
-B_vApp = squeeze(obj.vhclVapp.Data);
-% tangent pitch and roll
-tanPitch = squeeze(obj.tanPitch.Data)*180/pi;
-tanRoll = squeeze(obj.tanRoll.Data)*180/pi;
-% vhcl speed
-vhclSpeed = squeeze(obj.velocityVec.Data);
-vhclSpeed = vecnorm(vhclSpeed);
-% plot angle of attack
-AoA = squeeze(obj.vhclAngleOfAttack.Data);
+
+%% compute stats
 % lap time
 lapTime = squeeze(obj.currentPathVar.Time(ran));
 lapTime = lapTime(end)-lapTime(1);
-% average apparent velocity in x direction cubed
-meanVappxCubed = mean(max(0,B_vApp(1,:)).^3);
-% distance traveled
+
+% distance traveled over lap
 rCM = squeeze(obj.positionVec.Data);
 rCM = rCM(:,ran);
 disTraveled = sum(vecnorm(rCM(:,2:end) - rCM(:,1:end-1)));
-% average vCM
+
+% turbine power over lap
+turbPow = obj.turbPow.Data(:)./1e3;
+Avg_Power = mean(turbPow(ran));
+
+% V_a,x^3 by V_w^3 over lap
+B_vApp = squeeze(obj.vhclVapp.Data);
+flowAtFuse = squeeze(obj.vWindFuseGnd.Data(1,:,:))';
+vAppByvFlow = mean((max(0,B_vApp(1,ran))./flowAtFuse(ran)).^3);
+
+% V_k^3 by V_w^3 over lap
+vhclSpeed = squeeze(obj.velocityVec.Data);
+vhclSpeed = vecnorm(vhclSpeed);
+vKiteByvFlow = mean((vhclSpeed(ran)./flowAtFuse(ran)).^3);
+
+% Average speed
 avgVCM = mean(vhclSpeed(ran));
-% avereage speed
-avgSpeed = disTraveled/lapTime;
-% calculate derivative
-dv = diff(vhclSpeed(ran));
-dTanPitch = diff(tanPitch(ran));
-dTanRoll = diff(tanRoll(ran));
-dPath = diff(pathParam(ran));
-dvdp = dv(:)./dPath(:);
-dTpdp = dTanPitch(:)./dPath(:);
-dTanRoll = dTanRoll(:)./dPath(:);
 
-garbareRes = any(abs([dvdp dTpdp dTanRoll])>1e3,'all');
+% Avg. V_app,x^3
+vAppCubed = mean(max(0,B_vApp(1,ran)).^3);
 
-tanMax = max(abs(tanPitch(ran)),[],'all');
+% mean angle of attack
+AoA = squeeze(obj.vhclAngleOfAttack.Data);
+AoA = mean(AoA(ran));
 
-val = {'Lap no.','Lap time','Dist. traveled','(V_app,x)^2','Avg. V_cm',...
-    'Avg. tan pitch','Avg. AoA','garbage?','Max tanRoll?','max dv/dp',...
-    'max dTPitch/dp','max dTRoll/dp'};
-val(2,1:end-3) = {plotLap,lapTime,disTraveled,meanVappxCubed,avgVCM,...
-    mean(tanPitch(ran)),mean(AoA(ran)),garbareRes,tanMax};
-val(2,end-2:end) = num2cell(max(abs([dvdp dTpdp dTanRoll])));
+% max tangent roll
+tanRoll = squeeze(obj.tanRoll.Data)*180/pi;
+maxTan  = max(abs(tanRoll(ran)));
+
+val = {'Lap no.','Lap time','Dist. traveled','Avg. P','(V_app,x)^3',...
+    '(V_app,x/V_w)^3','(V_k/V_w)^3','Avg. V_cm',...
+    'Avg. AoA','Max roll'};
+val(2,1:end) = {plotLap,lapTime,disTraveled,Avg_Power,vAppCubed,...
+    vAppByvFlow,vKiteByvFlow,avgVCM,...
+   AoA,maxTan};
 
 end
