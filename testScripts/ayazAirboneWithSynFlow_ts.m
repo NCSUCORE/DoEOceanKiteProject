c4d5e6a3eb1cd0ccadd0bdc42c176955cd80da07
Ayaz <asiddiq2@ncsu.edu>
2020-11-20 19:45:50 -0500

    Refactored my test script to have simulation scenarios. Inspiration taken from John's scripts. Added a more realististic airborne vehicle design to the library.

--- testScripts/ayazAirboneWithSynFlow_ts.m
+++ testScripts/ayazAirboneWithSynFlow_ts.m
@@ -5,17 +5,20 @@ close all;
 cd(fileparts(mfilename('fullpath')));
 
 simParams = SIM.simParams;
-simParams.setDuration(5*60*60,'s');
+simParams.setDuration(0.1*60*60,'s');
 dynamicCalc = '';
 flowSpeed = 10;
 thrLength = 1000;
 % rad - Mean elevation angle
-initElevation = 30*pi/180;    
+initElevation = 30*pi/180;
 % rad - Path width/height
-w = 40*pi/180;          
-h = 8*pi/180;  
+w = 40*pi/180;
+h = 8*pi/180;
 % Path basis parameters
-[a,b] = boothParamConversion(w,h);      
+[a,b] = boothParamConversion(w,h);
+
+%% simulation scenarioes
+simScenario = [2 1 1 1 false];
 
 %% Load components
 % Spooling controller
@@ -32,32 +35,60 @@ loadComponent('ayazAirborneThr');
 loadComponent('idealSensors');
 % Sensor processing
 loadComponent('idealSensorProcessing');
-% Vehicle
-loadComponent('ayazAirborneVhcl');
 
-% High level controller
-% loadComponent('constBoothLem');
-% hiLvlCtrl.basisParams.setValue([a,b,initElevation,0*pi/180,thrLength],'[rad rad rad rad m]');
+% select vehicle based on sim scenario
+switch simScenario(1)
+    % Vehicle
+    case 1
+        loadComponent('ayazAirborneVhcl');
+    case 2
+        loadComponent('realisticAirborneVhcl');
+end
 
-loadComponent('gpkfPathOptAirborne');
-% hiLvlCtrl.maxStepChange        = (800/thrLength)*180/pi;
-hiLvlCtrl.maxStepChange        = 6;
-hiLvlCtrl.minVal               = 5;
-hiLvlCtrl.maxVal               = 50;
-hiLvlCtrl.basisParams.Value = [a,b,initElevation,0*pi/180,thrLength]';
-hiLvlCtrl.initVals          = hiLvlCtrl.basisParams.Value(3)*180/pi;
-hiLvlCtrl.rateLimit         = 1*0.15;
-hiLvlCtrl.kfgpTimeStep      = 10/60;
-hiLvlCtrl.mpckfgpTimeStep   = 3;
-predictionHorz  = 6;
-exploitationConstant = 1;
-explorationConstant  = 2^6;
+% select High level controller based on sim scenario
+switch simScenario(2)
+    case 1 % constant path shape
+        loadComponent('constBoothLem');
+        hiLvlCtrl.basisParams.setValue([a,b,initElevation,...
+            0*pi/180,thrLength],'[rad rad rad rad m]');
+    case 2 % only the high level control of mean elevation angle
+        loadComponent('gpkfPathOptAirborne');
+        % hiLvlCtrl.maxStepChange        = (800/thrLength)*180/pi;
+        hiLvlCtrl.maxStepChange        = 6;
+        hiLvlCtrl.minVal               = 5;
+        hiLvlCtrl.maxVal               = 50;
+        hiLvlCtrl.basisParams.Value = [a,b,initElevation,0*pi/180,thrLength]';
+        hiLvlCtrl.initVals          = hiLvlCtrl.basisParams.Value(3)*180/pi;
+        hiLvlCtrl.rateLimit         = 1*0.15;
+        hiLvlCtrl.kfgpTimeStep      = 10/60;
+        hiLvlCtrl.mpckfgpTimeStep   = 3;
+        predictionHorz  = 6;
+        exploitationConstant = 1;
+        explorationConstant  = 2^6;
+    case 3 % both mean elevation angle and path shape optimization
+        loadComponent('gpkfPathOptWithRGPAirborne');
+        % hiLvlCtrl.maxStepChange        = (800/thrLength)*180/pi;
+        hiLvlCtrl.maxStepChange        = 6;
+        hiLvlCtrl.minVal               = 5;
+        hiLvlCtrl.maxVal               = 50;
+        hiLvlCtrl.basisParams.Value = [a,b,initElevation,0*pi/180,thrLength]';
+        hiLvlCtrl.initVals          = hiLvlCtrl.basisParams.Value(3)*180/pi;
+        hiLvlCtrl.rateLimit         = 1*0.15;
+        hiLvlCtrl.kfgpTimeStep      = 10/60;
+        hiLvlCtrl.mpckfgpTimeStep   = 3;
+        predictionHorz  = 6;
+        exploitationConstant = 1;
+        explorationConstant  = 2^6;
+end
 
-% Environment
-loadComponent('ayazAirborneSynFlow');
-
-% loadComponent('ayazAirborneFlow.mat');
-% env.water.flowVec.setValue([flowSpeed;0;0],'m/s');
+% select Environment based on sim scenario
+switch simScenario(3)
+    case 1
+        loadComponent('ayazAirborneFlow.mat');
+        env.water.flowVec.setValue([flowSpeed;0;0],'m/s');
+    case 2
+        loadComponent('ayazAirborneSynFlow');
+end
 
 cIn = maneuverabilityAdvanced(vhcl);
 cIn.meanElevationInRadians = initElevation;
@@ -67,8 +98,8 @@ cIn.tetherLength = thrLength;
 minCur = cIn.calcMinCurvature;
 pLength = cIn.pathLength;
 
-figure(10);
-pR = cIn.plotPathRadiusOfCurvature;
+% figure(10);
+% pR = cIn.plotPathRadiusOfCurvature;
 
 %% Ground Station IC's and dependant properties
 gndStn.setPosVec([0 0 0],'m')
@@ -97,8 +128,12 @@ thr.tether1.vehicleMass.setValue(vhcl.mass.Value,'kg');
 %% Winches IC's and dependant properties
 wnch.setTetherInitLength(vhcl,gndStn.posVec.Value,env,thr,[flowSpeed;0;0]);
 
-%% Run Simulation
-loadComponent('ayazPathFollowingAirborne');
+%% path following controller
+% select path following controller based on sim scenario
+switch simScenario(4)
+    case 1
+        loadComponent('ayazPathFollowingAirborne');
+end
 fltCtrl.setFcnName(PATHGEOMETRY,'');
 fltCtrl.setInitPathVar(vhcl.initPosVecGnd.Value,...
     hiLvlCtrl.basisParams.Value,...
@@ -108,6 +143,7 @@ fltCtrl.elevatorReelInDef.setValue(0,'deg');
 fltCtrl.rudderGain.setValue(0,'');
 fltCtrl.yawMoment.kp.setValue(0,'(N*m)/(rad)');
 
+%% Run Simulation
 keyboard
 simWithMonitor('OCTModel');
 
@@ -116,27 +152,87 @@ statOld = computeSimLapStats(tscOld);
 trackOld = statOld{2,3}/cIn.pathLength;
 
 %% omniscient
-[synFlow,synAlt] = env.water.generateData();
-elevsAtAllAlts = min(max(hiLvlCtrl.minVal,...
-    asin(env.water.zGridPoints.Value/thrLength)*180/pi),hiLvlCtrl.maxVal);
-omniAlts = thrLength*sind(elevsAtAllAlts);
-cosElevAtAllAlts = cosd(elevsAtAllAlts);
-
-tSamp = 0:hiLvlCtrl.mpckfgpTimeStep:simParams.duration.Value/60;
-
-for ii = 1:length(tSamp)
-    % measure flow at xSamp(ii) at tSamp(ii)
-    fData = resample(synFlow,tSamp(ii)*60).Data;
-    hData = resample(synAlt,tSamp(ii)*60).Data;
-    % calculate pseudo power
-    % omniscient, uncontrained controller
-    omnifData = interp1(hData,fData,omniAlts);
-    [fValOmni(ii),omniIdx] = max(cosineFlowCubed(omnifData,cosElevAtAllAlts));
-    runAvgOmni(ii) = mean(fValOmni(1:ii));
-    omniElev(ii) = elevsAtAllAlts(omniIdx);
+if ismember(simScenario(2),[2,3])
+    [synFlow,synAlt] = env.water.generateData();
+    elevsAtAllAlts = min(max(hiLvlCtrl.minVal,...
+        asin(env.water.zGridPoints.Value/thrLength)*180/pi),hiLvlCtrl.maxVal);
+    omniAlts = thrLength*sind(elevsAtAllAlts);
+    cosElevAtAllAlts = cosd(elevsAtAllAlts);
+    
+    tSamp = 0:hiLvlCtrl.mpckfgpTimeStep:simParams.duration.Value/60;
+    
+    for ii = 1:length(tSamp)
+        % measure flow at xSamp(ii) at tSamp(ii)
+        fData = resample(synFlow,tSamp(ii)*60).Data;
+        hData = resample(synAlt,tSamp(ii)*60).Data;
+        % calculate pseudo power
+        % omniscient, uncontrained controller
+        omnifData = interp1(hData,fData,omniAlts);
+        [fValOmni(ii),omniIdx] = max(cosineFlowCubed(omnifData,...
+            cosElevAtAllAlts));
+        runAvgOmni(ii) = mean(fValOmni(1:ii));
+        omniElev(ii) = elevsAtAllAlts(omniIdx);
+    end
+    
 end
 
-%% table headers
+%% plot figures
+switch simScenario(2)
+    case 1
+        plotFigs = {'Tangent roll','Speed','Apparent vel. in x cubed',...
+            'Turbine power','Kite speed by flow speed cubed',...
+            'Flow at kite','Lap stats'};
+    case 2
+        plotFigs = {'Tangent roll','Speed','Apparent vel. in x cubed',...
+            'Turbine power','Kite speed by flow speed cubed',...
+            'Flow at kite','Path elevation angle'};
+end
+
+for ii = 1:length(plotFigs)
+    plotSomething(tscOld,plotFigs{ii},1);
+end
+
+if ismember(simScenario(2),[2,3])
+    fh = findobj('Type','Figure','Name','Path elevation angle');
+    figure(fh);
+    plot(tSamp*60,omniElev,'r-');
+    legend('Simulation','Omniscient offline');
+end
+
+allAxes = findall(0,'type','axes');
+allPlots = findall(0,'type','Line');
+
+set(allPlots,'linewidth',1);
+set(allAxes,'FontSize',12);
+
+%% save figures
+if simScenario(5)
+    for ii = 1:length(plotFigs)
+        figure(ii);
+        exportgraphics(gcf,[plotFigs{ii},'.png'],'Resolution',600);
+    end
+end
+
+
+%% animations
+GG.saveGifs = true;
+GG.timeStep = 1;
+GG.gifTimeStep = 0.1;
+
+switch simScenario(2)
+    case 1
+        vhcl.animateSim(tscOld,GG.timeStep,...
+            'PathFunc',fltCtrl.fcnName.Value,'pause',false,'plotTarget',false,...
+            'SaveGif',GG.saveGifs,'GifTimeStep',GG.gifTimeStep,...
+            'GifFile','AirGif.gif','plotFlowShearProfile',false,'plotTracer',false);
+    case 2
+        vhcl.animateSim(tscOld,GG.timeStep,...
+            'PathFunc',fltCtrl.fcnName.Value,'pause',false,'plotTarget',false,...
+            'SaveGif',GG.saveGifs,'GifTimeStep',GG.gifTimeStep,...
+            'GifFile','AirGifGPKF.gif','plotFlowShearProfile',true,'plotTracer',false);
+end
+
+%% write data to table
 headers = {'Mean elevation','Path width','Path height',...
     'Path length',...
     'Lap no.','Lap time','Dist. traveled','Avg. P','(V_app,x)^3',...
@@ -157,111 +253,19 @@ baseTable(1,3).("Path height") = cIn.pathHeight;
 baseTable(1,4).("Path length") = cIn.pathLength;
 baseTable(1,5:end) = {statOld{2,:}, trackOld};
 
-% writetable(baseTable,'manualMode.xlsx',"WriteMode","append");
+if simScenario(5)
+    writetable(baseTable,'manualMode.xlsx',"WriteMode","append");
+end
 
-%%
-close all
-fn = 1;
-figure(fn)
-pOSP = plot(tscOld.tanRollDes.Time,tscOld.tanRollDes.Data(:)*180/pi,'b-');
-hold on
-plot(tscOld.tanRoll.Time,tscOld.tanRoll.Data*180/pi,'b--');
-ylabel('Tangent roll [deg]');
-legend('Roll SP','Roll');
-
-fn = fn+1;
-figure(fn);
-plot(tscOld.tanRoll.Time,squeeze(vecnorm(tscOld.velCMvec.Data)),'b-');
-hold on;
-ylabel('Speed [m/s]');
-
-fn = fn+1;
-figure(fn);
-plot(tscOld.tanRoll.Time,tscOld.vhclVapp.Data(1,:).^3,'b-');
-hold on;
-ylabel('Apparent vel. in x cubed [$m^3/s^3$]');
-
-fn = fn+1;
-figure(fn);
-plot(tscOld.tanRoll.Time,tscOld.turbPow.Data(:)./1e3,'b-');
-hold on;
-ylabel('Power [kW]');
-
-fn = fn+1;
-figure(fn);
-plot(tscOld.tanRoll.Time,(squeeze(vecnorm(tscOld.velCMvec.Data))./...
-    squeeze(tscOld.vWindFuseGnd.Data(1,1,:))).^3,'b-');
-hold on;
-ylabel('$(V_{k}/V_{w})^3$ [-]');
-
-fn = fn+1;
-figure(fn);
-plot(tscOld.tanRoll.Time,squeeze(tscOld.vWindFuseGnd.Data(1,1,:)),'b-');
-hold on;
-ylabel('Flow speed at kite [m/s]');
-
-fn = fn+1;
-figure(fn);
-% plot(tscOld.tanRoll.Time,squeeze(tscOld.elevationAngle.Data(:)),'r');
-plot(tSamp*60,omniElev,'r-');
-hold on;
-plot(tscOld.tanRoll.Time,squeeze(tscOld.basisParams.Data(:,3)*180/pi),'b-');
-ylabel('Path mean elevation angle [m/s]');
-legend('Omniscient offline','Simulation')
-
-% fn = fn+1;
-% figure(fn);
-% plot(tscOld.tanRoll.Time,squeeze(tscOld.numericalLapAvgPerformace.Data(:)),'b-');
-% hold on;
-% ylabel('Lap averaged $(V_{k}/V_{w})^3$');
-
-% fn = fn+1;
-% figure(fn);
-% subplot(2,1,1)
-% plot(tscOld.pathWidth_deg.Time,squeeze(tscOld.pathWidth_deg.Data(:)),'b-');
-% hold on;
-% ylabel('Path width [deg]');
-% subplot(2,1,2)
-% plot(tscOld.pathHeight_deg.Time,squeeze(tscOld.pathHeight_deg.Data(:)),'b-');
-% hold on;
-% ylabel('Path height [deg]');
+%% save the file
+if simScenario(5)
+    filename = strcat(cd,'\',sprintf('SynFlowRes_Date-'),...
+        strrep(datestr(datetime),':','-'),'.mat');
+    save(filename,'tscOld','vhcl','thr','fltCtrl','env','simParams','gndStn',...
+        'hiLvlCtrl');
+end
 
 
-allAxes = findall(0,'type','axes');
-allPlots = findall(0,'type','Line');
-xlabel(allAxes,'Time [sec]');
-grid(allAxes,'on');
-
-
-set(allPlots,'linewidth',1);
-set(allAxes,'FontSize',12);
-xlim(allAxes,[0 tscOld.tanRoll.Time(end)]);
-figNames = {'desTanRoll','speed','pSurrog','pTurb','vkByVw','flowAtKite',...
-'elevationAngle'};
-% ,'lapAvgPerf','pathShape'};
-
-% for ii = 1:fn
-%     figure(ii);
-%     exportgraphics(gcf,[figNames{ii},'.png'],'Resolution',600);
-% end
-
-
-
-%%
-GG.saveGifs = true;
-GG.timeStep = 2.5;
-GG.gifTimeStep = 0.1;
-
-vhcl.animateSim(tscOld,GG.timeStep,...
-    'PathFunc',fltCtrl.fcnName.Value,'pause',false,'plotTarget',false,...
-    'SaveGif',GG.saveGifs,'GifTimeStep',GG.gifTimeStep,...
-    'GifFile','AirOldGif.gif','plotFlowShearProfile',false,'plotTracer',false);
-
-filename = strcat(cd,'\',sprintf('SynFlowRes_Date-'),...
-    strrep(datestr(datetime),':','-'),'.mat');
-%%
-save(filename,'tscOld','vhcl','thr','fltCtrl','env','simParams','gndStn',...
-    'hiLvlCtrl');
 
 
 
