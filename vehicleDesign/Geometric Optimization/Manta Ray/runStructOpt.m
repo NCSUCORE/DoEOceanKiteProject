dd10b26e1d7a9a4c82ae31ce1bda29f30a769ca5
John <jborek@uncc.edu>
2020-12-11 10:56:33 -0500

    Updated my structural analysis tool - JB

--- vehicleDesign/Geometric Optimization/Manta Ray/runStructOpt.m
+++ vehicleDesign/Geometric Optimization/Manta Ray/runStructOpt.m
@@ -1,31 +1,29 @@
-function [Ixx_opt,Fthk,Mtot,Wingdim] = runStructOpt(vhcl,wing,hStab,vStab,fuse,Env)
+function [Ixx_calc,Ixx_req,Fthk,Mtot,Wingdim] = runStructOpt(wing,hStab,vStab,fuse,Env,loads)
 
-chord = vhcl.portWing.MACLength.Value;
-span = vhcl.portWing.halfSpan.Value*2;
-AR = vhcl.portWing.AR.Value*2;
-volume = chord*span^3/AR^2;
-Df = vhcl.fuse.diameter.Value;
-Lf = vhcl.fuse.length.Value;
-alph = -13.5:.5:13.5;
-Flift = NaN*ones(numel(alph),1);
-CL = NaN*ones(numel(alph),1);
-CD = NaN*ones(numel(alph),1);
-for i = 1:numel(alph)
-    [~,Flift(i),CL(i),CD(i)] = wingPowerCost([AR,alph(i)],wing,hStab,vStab,fuse,Env);
-end
-Fmax = max(Flift);
-delx = span/4*.05;
-Ixx_req = (39.37^4)*5*(0.9)*Fmax*(span/4)^3/(48*69e9*delx);
-Skmax = 0.2; Sp1max = 0.2; 
-[Ixx_calc] = App_Wing_MoICalc_old(chord, Skmax, Sp1max,Sp1max,Sp1max);
+chord = wing.c;
+span = wing.b;
+AR = wing.AR;
+volume = chord*span^3/AR^2/2;
+Df = fuse.D;
+Lf = fuse.L;
+
+P = loads.Wp;
+L = span/2;
+a = span/4;
+delx = span/2*.05;
+Ixx_req = (39.37^4)*P*a^3*(3*L-a)/(6*wing.E*delx);
+
+Skn = 0.025; Sp1 = 0.04; Sp2 = 0.02; 
+[Ixx_calc,area_skin,area_spar1,area_spar2] = App_Wing_MoICalc_old(chord,Skn,Sp1,Sp2);
 Ixx_calc = Ixx_calc*(39.37^4);
-if Ixx_calc > Ixx_req
-    [Ixx_opt,Mwing,exitflagW,Wopt] = App_SWDT2(AR,span,volume,Ixx_req,Df,Lf);
-    if exitflagW == 1
-        [Mfuse,Fthk,~] = App_SFDT2(Df,Lf,Fmax);
-        Mtot = Mfuse+Mwing;
-        Wingdim = Wopt;
-    end
-end
+area_opt = area_skin+area_spar1+area_spar2;
+% Mwing = area_opt*S_opt*in.rhow;
+
+% [Mfuse,Fthk,~] = App_SFDT2(Df,Lf,Fmax);
+% Mtot = Mfuse+Mwing;
+% Wingdim = Wopt;
+Fthk = 0;
+Mtot = 0;
+Wingdim = 0;
 end
 
