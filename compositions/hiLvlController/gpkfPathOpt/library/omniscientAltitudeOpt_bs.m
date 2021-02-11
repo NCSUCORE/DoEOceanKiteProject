c2015273c1c0ab13d9ae1a031d4cc7375adec3ac
Ayaz <asiddiq2@ncsu.edu>
2021-02-11 01:43:29 -0500

    made controller for omniscient altitude optimization.

---
+++ compositions/hiLvlController/gpkfPathOpt/library/omniscientAltitudeOpt_bs.m
@@ -0,0 +1,25 @@
+clear
+clc
+close all
+
+HILVLCONTROLLER = 'omniscientAltitudeOpt';
+PATHGEOMETRY = 'lemOfBooth';
+
+loadComponent('ayazAirborneSynFlow');
+loadComponent('gpkfAltitudeOptimization');
+
+[synFlow,synAlt] = env.water.generateData();
+
+simTime = 1*60*60;
+
+initVal = 0.5*(hiLvlCtrl.minVal + hiLvlCtrl.maxVal);
+initVal = 500;
+
+altSPTraj = calculateOmniAltitudeSPTraj(synAlt,synFlow,hiLvlCtrl,...
+    initVal,simTime);
+
+hiLvlCtrl.altSPTraj = altSPTraj;
+
+saveFile = saveBuildFile('hiLvlCtrl',mfilename,'variant','HILVLCONTROLLER');
+save(saveFile,'PATHGEOMETRY','-append')
+
