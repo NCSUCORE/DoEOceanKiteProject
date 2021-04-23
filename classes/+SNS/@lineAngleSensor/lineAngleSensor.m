559f45c34058507fa9805e0389c87103c89eadfd
adabney89 <adabney@ncsu.edu>
2021-04-23 18:25:21 -0400

    Integrated LAS into the simulink model. Tether forces are not correctly accounted for yet. Need to improve this integration. Currently can predict LAS elevation based on a CD. Need to figure out the geometry of the applied forces at the LAS. Haven't seen a clear formulation of this yet.

---
+++ classes/+SNS/@lineAngleSensor/lineAngleSensor.m
@@ -0,0 +1,105 @@
+classdef lineAngleSensor < dynamicprops
+    %REALISTIC container for instances of realistic sensor models
+    
+    properties
+    % Dynamics Paramters
+    mass
+    volume
+    diameter
+    length
+    Ixx
+    Iyy
+    Izz
+    L_CB
+    L_CM
+    CD
+    dragEnable
+    tetherEnable
+    initAng
+    initAngVel
+    end
+    
+    methods
+        function obj = lineAngleSensor()
+            obj.mass            = SIM.parameter('Unit','kg','Description','Line Angle Sensor Mass');
+            obj.volume          = SIM.parameter('Unit','m^3','Description','Line Angle Sensor Volume');
+            obj.diameter        = SIM.parameter('Unit','m','Description','Line Angle Boom Diameter');
+            obj.length          = SIM.parameter('Unit','m','Description','Line Angle Sensor Pendulum Length');
+            obj.Ixx             = SIM.parameter('Unit','kg*m^2','Description','Line Angle Sensor Inertia about the X Axis');
+            obj.Iyy             = SIM.parameter('Unit','kg*m^2','Description','Line Angle Sensor Inertia about the Y Axis');
+            obj.Izz             = SIM.parameter('Unit','kg*m^2','Description','Line Angle Sensor Inertia about the Z Axis');
+            obj.L_CM            = SIM.parameter('Unit','m','Description','Linear Distance to the Center of Mass from pivot');
+            obj.L_CB            = SIM.parameter('Unit','m','Description','Linear Distance to the Center of Buoyancy from pivot');
+            obj.CD              = SIM.parameter('Unit','','Description','Coefficient of Drag');
+            obj.dragEnable      = SIM.parameter('Value',1,'Description','Enable Line Angle Sensor Drag');
+            obj.tetherEnable    = SIM.parameter('Value',1,'Description','Enable Tether Loading');
+            obj.initAng         = SIM.parameter('Unit','rad','Description','Initial Angles');
+            obj.initAngVel      = SIM.parameter('Value',[0 0],'Unit','rad/s','Description','Initial Angular Velocities');
+        end
+        
+        function obj = setMass(obj,val,unit)
+            obj.mass.setValue(val,unit);
+        end
+        
+        function obj = setVolume(obj,val,unit)
+            obj.volume.setValue(val,unit);
+        end
+        
+        function obj = setDiameter(obj,val,unit)
+            obj.diameter.setValue(val,unit);
+        end
+        
+        function obj = setLength(obj,val,unit)
+            obj.length.setValue(val,unit);
+        end
+        
+        function obj = setIxx(obj,val,unit)
+            obj.Ixx.setValue(val,unit);
+        end
+        
+        function obj = setIyy(obj,val,unit)
+            obj.Iyy.setValue(val,unit);
+        end 
+
+        function obj = setIzz(obj,val,unit)
+            obj.Izz.setValue(val,unit);
+        end
+        
+        function obj = setL_CM(obj,val,unit)
+            obj.L_CM.setValue(val,unit);
+        end
+        
+        function obj = setL_CB(obj,val,unit)
+            obj.L_CB.setValue(val,unit);
+        end
+        
+        function obj = setCD(obj,val,unit)
+            obj.CD.setValue(val,unit);
+        end
+        
+        function obj = dragEnableOn(obj)
+            obj.dragEnable.setValue(1,'');
+        end
+        
+        function obj = dragDisable(obj)
+            obj.dragEnable.setValue(0,'');
+        end
+        
+        function obj = tetherLoadEnable(obj)
+            obj.tetherEnable.setValue(1,'')
+        end
+        
+        function obj = tetherLoadDisable(obj)
+            obj.tetherEnable.setValue(0,'')
+        end
+        
+        function obj = setInitAng(obj,val,unit)
+            obj.initAng.setValue(val,unit);
+        end
+        
+        function obj = setInitAngVel(obj,val,unit)
+            obj.initAngVel.setValue(val,unit);
+        end
+    end
+end
+
