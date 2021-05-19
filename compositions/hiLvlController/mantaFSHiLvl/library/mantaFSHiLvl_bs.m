880c021e73c2dcaa106a80d88480662c9f0a27b6
John <56124877+jborek51@users.noreply.github.com>
2021-05-19 14:58:24 -0400

    updated the high level full cycle controller to include a hardcoded state selection. Cleaned up the constant basis parameters high-level controller and began working on the full cycle flight controller. Added the unit library for taking the magnitude of vector - JB

--- compositions/hiLvlController/mantaFSHiLvl/library/mantaFSHiLvl_bs.m
+++ compositions/hiLvlController/mantaFSHiLvl/library/mantaFSHiLvl_bs.m
@@ -13,6 +13,11 @@ hiLvlCtrl.basisParams.setValue([.73,.6,.36,0,125],'[rad rad rad rad m]');
 hiLvlCtrl.add('GainNames',{'maxNumberOfSimulatedLaps'},'GainUnits',{''});
 hiLvlCtrl.maxNumberOfSimulatedLaps.setValue(inf,'');
 
+hiLvlCtrl.add('GainNames',{'stateCtrl'},'GainUnits',{''});
+hiLvlCtrl.stateCtrl.setValue(1,'');
+
+hiLvlCtrl.add('GainNames',{'stateConst'},'GainUnits',{''});
+hiLvlCtrl.stateConst.setValue(1,'');
 
 %% save file in its respective directory
 saveFile = saveBuildFile('hiLvlCtrl',mfilename,'variant','HILVLCONTROLLER');
