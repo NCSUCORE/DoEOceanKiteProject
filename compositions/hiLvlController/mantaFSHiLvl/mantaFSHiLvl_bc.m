7bcb2682c351b898f98b692b76d314c13170af66
John <56124877+jborek51@users.noreply.github.com>
2021-05-19 19:13:16 -0400

    The non cross-current modual works. Sinal loing is a nihtmare so I'll be looking into that. I won't continue work until this weekend btw. I also modified the turbine block to output zero power for static rotors  - JB

--- compositions/hiLvlController/mantaFSHiLvl/mantaFSHiLvl_bc.m
+++ compositions/hiLvlController/mantaFSHiLvl/mantaFSHiLvl_bc.m
@@ -13,6 +13,16 @@ elems(1).SampleTime = -1;
 elems(1).Complexity = 'real';
 elems(1).Unit = '';
 
+elems(2) = Simulink.BusElement;
+elems(2).Name = 'stateMac';
+elems(2).Dimensions = 1;
+elems(2).DimensionsMode = 'Fixed';
+elems(2).DataType = 'double';
+elems(2).SampleTime = -1;
+elems(2).Complexity = 'real';
+elems(2).Unit = '';
+
+
 CONTROL = Simulink.Bus;
 CONTROL.Elements = elems;
 CONTROL.Description = 'Basis parameters';
