a0e0a481b8b102ff50786f1d105f8c91bc30f9e8
adabney89 <adabney@ncsu.edu>
2021-05-28 12:47:52 -0400

    Revert "Updated test script and line angle sensor with debug issue. Added LAS signals to the realistic sensor processing bus"
    This reverts commit 942eecfa8736968253e4df5a569cddd918d9271a.

--- testScripts/poolRaft/PostProcess_ts.m
+++ testScripts/poolRaft/PostProcess_ts.m
@@ -35,7 +35,7 @@ for i = 1:length(inc)
             loadComponent('MantaTether');                               %   Manta Ray tether
             loadComponent('realisticSensors');                             %   Sensors
             loadComponent('lineAngleSensor');
-            loadComponent('realisticSensorProcessing');                      %   Sensor processing
+            loadComponent('idealSensorProcessing');                      %   Sensor processing
             loadComponent('poolScaleKiteAbney');                %   AR = 8; 8m span
             SIXDOFDYNAMICS        = "sixDoFDynamicsCoupledFossen12int";
             %%  Environment Properties
@@ -204,15 +204,6 @@ for i = 18
     plot(tsc.speedCMD1.Time(a:end),tsc.speedCMD1.Data(a:end))
 end
 
-a = squeeze(diff(sqrt(dot(tsc1{1,1}.velCMvec.Data,tsc1{1,1}.velCMvec.Data))))...
-    ./(tsc1{1,1}.azAng.Time(2:end)-tsc1{1,1}.azAng.Time(1:end-1));
-T = tsc1{1,1}.airTenVecs.Time(1:end-1)
-
-figure
-plot(T,a)
-ylim([-3 3])
-
-
 figure; hold on; grid on;
 plotsq(tsc1{1,1}.velCMvec.Time,sqrt(dot(tsc1{1,1}.velCMvec.Data,tsc1{1,1}.velCMvec.Data)))
 plotsq(tsc1{1,1}.velEst.Time,sqrt(dot(tsc1{1,1}.velEst.Data',tsc1{1,1}.velEst.Data')))
@@ -244,7 +235,7 @@ subplot(3,1,3); hold on; grid on;
 plotsq(tsc1{1,1}.velocityVec.Time,tsc1{1,1}.velocityVec.Data(3,:,:))
 plotsq(tsc1{1,1}.velocityVec.Time,tsc1{1,1}.velEst.Data(:,3,:))
 ylim([-2,2])
-legend('Velocity','Velocity Estimation')
+legend('Pos','Pos Estimation')
 
 %% March 26 Data
 towArray = [0.47 0.62 0.77]*1
