eb26b7e87ad96ee3492563c7016c244b3b871c09
adabney89 <adabney@ncsu.edu>
2021-06-16 13:52:54 -0400

    updated TS and data processing method

--- functions/estExpVelMag.m
+++ functions/estExpVelMag.m
@@ -1,9 +1,10 @@
-function [velMag,velAug] = estExpVelMag(tsc,towSpeed,cutOffFreq)
+function [velMag,velAug] = estExpVelMag(tsc,cutOffFreq)
 %Grab Time and Position Angles from Time Series
 T = tsc.kite_azi.Time;
 sampPeriod = T(2)-T(1);
 el = squeeze(tsc.kite_elev.Data);
 az = squeeze(tsc.kite_azi.Data);
+towSpeed = tsc.winch1rpm.Data(100)*.0098
 
 %Estimate Position based on straight tether approximation
 pos = 2.63*[-cosd(az).*cosd(el)+towSpeed/2.63*T...
