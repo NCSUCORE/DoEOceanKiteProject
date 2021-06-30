8a9314be3da27f3956e4706b84d8fa7d1433dfc1
jborek51 <john.borek.51@gmail.com>
2021-06-30 13:44:48 -0400

    Updated drag coefficients and controllers - JB

--- compositions/plants/components/tethers/tetherM/library/MantaFSTether/MantaFSTether_bs.m
+++ compositions/plants/components/tethers/tetherM/library/MantaFSTether/MantaFSTether_bs.m
@@ -6,19 +6,19 @@ TETHERS = 'tetherM';
 thr = OCT.tetherM;                     % Create the tether
 
 %% Set tether properties (can be reset in test script)
-thr.maxTetherLength.setValue(600,'m')       % Max tether that is avalible total
-thr.setLinkLength      (60,'m')          % Total nodes in tether
-thr.setNominalDrag     (1,'')          % Drag coeff on tether
-thr.setFairingDrag     (.1,'')          % Drag coeff on tether with farrings
-thr.setFairingLength   (120,'m')       % Fairing length measured from kite
-thr.setDiameter        (0.018,'m')    % Total tether diameter
-thr.setYoungsMod       (57e9,'Pa')     % Total tether Youngs Modulus
-thr.setDampingRatio    (.1,'')          % Tether damping ratio
-thr.setDensity         (1500,'kg/m^3') % Total tether density
-thr.setMinLinkLength   (1,'m')         % Minimum individual link length (increasing this
+thr.maxTetherLength.setValue(600,'m');       % Max tether that is avalible total
+thr.setLinkLength      (60,'m');          % Total nodes in tether
+thr.setNominalDrag     (1.2,'');          % Drag coeff on tether
+thr.setFairingDrag     (.1,'');          % Drag coeff on tether with farrings
+thr.setFairingLength   (120,'m');       % Fairing length measured from kite
+thr.setDiameter        (0.018,'m');    % Total tether diameter
+thr.setYoungsMod       (57e9,'Pa');     % Total tether Youngs Modulus
+thr.setDampingRatio    (.1,'');          % Tether damping ratio
+thr.setDensity         (1500,'kg/m^3'); % Total tether density
+thr.setMinLinkLength   (1,'m');         % Minimum individual link length (increasing this
                                        % value can help with stiffness seen at short link
                                        % lengths)
-thr.setInitTetherLength(600,'m')       % Initial total tether length
+thr.setInitTetherLength(600,'m');       % Initial total tether length
 
 val = thr.maxTetherLength.Value/thr.linkLength.Value+1;
 if mod(val,1)~=0
@@ -26,7 +26,7 @@ if mod(val,1)~=0
 elseif thr.fairingLength.Value <= thr.linkLength.Value
     error('Link length is to large')
 else
-    thr.numNodes.setValue(val,'')
+    thr.numNodes.setValue(val,'');
 end
 
 %% Save file in its respective directory
