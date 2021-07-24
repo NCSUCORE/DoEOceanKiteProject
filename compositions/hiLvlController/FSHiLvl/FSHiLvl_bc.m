3995cadde018000a2fba6b75723838c3a2822fce
adabney89 <adabney@ncsu.edu>
2021-07-24 09:27:42 -0400

    renamed hi level controller based on defining function not project -AA

---
+++ compositions/hiLvlController/FSHiLvl/FSHiLvl_bc.m
@@ -0,0 +1,32 @@
+function mantaFSHiLvl_bc()
+
+% Creates output bus used by 
+elems(1) = Simulink.BusElement;
+elems(1).Name = 'basisParams';
+try
+elems(1).Dimensions = 5;
+catch
+end
+elems(1).DimensionsMode = 'Fixed';
+elems(1).DataType = 'double';
+elems(1).SampleTime = -1;
+elems(1).Complexity = 'real';
+elems(1).Unit = '';
+
+elems(2) = Simulink.BusElement;
+elems(2).Name = 'stateMac';
+elems(2).Dimensions = 1;
+elems(2).DimensionsMode = 'Fixed';
+elems(2).DataType = 'double';
+elems(2).SampleTime = -1;
+elems(2).Complexity = 'real';
+elems(2).Unit = '';
+
+
+CONTROL = Simulink.Bus;
+CONTROL.Elements = elems;
+CONTROL.Description = 'Basis parameters';
+
+assignin('base','hiLvlCtrlBus',CONTROL)
+
+end
