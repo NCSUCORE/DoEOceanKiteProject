50b94ba183cd3621fca79a89de8a4ffbaeddf1e3
Jacob Fine <jbfine@ncsu.edu>
2023-11-14 10:30:32 -0500

    Added additional comments to the demo testscript and made small changes for clarity

--- testScripts/pathFollowingDemo_ts.m
+++ testScripts/pathFollowingDemo_ts.m
@@ -2,21 +2,16 @@ clear all;
 clc;
 % close all;
 Simulink.sdi.clear
-%% Simulation Setup
-% 1 - Vehicle Model:         1 = AR8b8, 2 = AR9b9, 3 = AR9b10
-% 2 - High-level Controller: 1 = const basis, 2 = const basis/stategit  flow
-% 3 - Flight controller:     1 = pathFlow, 2 = full cycle
-% 4 - Tether Model:          1 = Single link, 2 = Reel-in, 3 = Multi-node, 4 = Multi-node faired
-% 5 - Environment:           1 = const flow, 2 = variable flow
-% 6 - Save Results
-% 7 - Animate
-% 8 - Plotting
-%%             1 2 3 4 5 6     7   
-simScenario = [1 1 1 3 1 1==0  1==1];
 
+saveSim=0;%Binary flag to save sim results
+animateFlag=1;% Binary flag to shown an animation post-sim
+
+%Simulation sweep parameters (all of these can be vectors to simulate multiple scenerios sequentially)
 thrSweep = 500;%Tether Lengths to Sweep Through
 altSweep = 250;%Altitudes to sweep through
 flwSweep = 1;%Flow Speed to simulate
+
+%Create empty vectors to populate with sim results
 x = meshgrid(thrSweep,altSweep,flwSweep);
 [n,m,r] = size(x);
 numCase = n*m*r;
@@ -24,13 +19,16 @@ powGen = zeros(n,m,r);
 pathErr = zeros(n,m,r);
 dragRatio = zeros(n,m,r);
 Pow = cell(n,m,r);
+
 fpath = 'C:\Users\';%Save Path for simulations
-%%
-if ~exist(fpath,'dir') && simScenario(6)
+
+%Make save directory if it does not exist
+if ~exist(fpath,'dir') && saveSim
     mkdir(fpath)
 
 end
 
+%Sweep through all the simulation scenarios
 for i = 1:n
     if i < 1
         continue
@@ -43,13 +41,13 @@ for i = 1:n
             if k < 1
                 continue
             end
-            
+            %Print sim sweep progress
             fprintf(sprintf('%.2f Percent Complete\n',((i-1)*m*r+(j-1)*r+k)/(n*m*r)*100))
             Simulink.sdi.clear
             %%  Set Test Parameters
-            tFinal = 5000;      tSwitch = 10000;                        %   s - maximum sim duration
+            tFinal = 1000;      tSwitch = 10000;                        %   s - maximum sim duration
             flwSpd = flwSweep(k);                                              %   m/s - Flow speed
-            altitude = thrSweep(j)/2;%altSweep(i);     initAltitude = 100;                     %   m/m - cross-current and initial altitude
+            altitude = altSweep(i);                        %   m/m - cross-current and initial altitude
             thrLength = thrSweep(j);    initThrLength =  thrSweep(j);
             el = asin(altitude/thrLength);                              %   rad - Initial elevation angle
            
@@ -94,16 +92,8 @@ for i = 1:n
             PATHGEOMETRY = 'lemBoothNew';   % Set desired path shape (figure-8)
             
             %Initalize vehicle states
-            if simScenario(3) == 1
-                if simScenario(2) == 4
-                    vhcl.setICsOnPath(0.875,PATHGEOMETRY,hiLvlCtrl.initBasisParams.Value,gndStn.posVec.Value,3*flwSpd)
-                else
-                    vhcl.setICsOnPath(0.875,PATHGEOMETRY,hiLvlCtrl.basisParams.Value,gndStn.posVec.Value,3*flwSpd)
-                end
-            else
-                vhcl.setICsOnPath(0,PATHGEOMETRY,hiLvlCtrl.basisParams.Value,gndStn.posVec.Value,0)
-                vhcl.setInitEulAng([0,0,0]*pi/180,'rad')
-            end
+            vhcl.setICsOnPath(0.875,PATHGEOMETRY,hiLvlCtrl.basisParams.Value,gndStn.posVec.Value,3*flwSpd)
+
             %%  Tether
             % Initalize tether position
             thr.tether1.initGndNodePos.setValue(gndStn.thrAttch1.posVec.Value(:)+gndStn.posVec.Value(:),'m');
@@ -114,17 +104,14 @@ for i = 1:n
             
             %Set Tether properties
             thr.tether1.vehicleMass.setValue(vhcl.mass.Value,'kg');
-            thr.tether1.dragCoeff.setValue(1.2,'')
+            thr.tether1.dragCoeff.setValue(1.2,'');
             %%  Winches Properties
             %Initalize winch
             wnch.setTetherInitLength(vhcl,gndStn.posVec.Value,env,thr,env.water.flowVec.Value);
             %%  Controller User Def. Parameters and dependant properties
             fltCtrl.setFcnName(PATHGEOMETRY,'');
-            if simScenario(2) == 4
-                fltCtrl.setInitPathVar(vhcl.initPosVecGnd.Value,hiLvlCtrl.initBasisParams.Value,gndStn.posVec.Value);
-            else
-                fltCtrl.setInitPathVar(vhcl.initPosVecGnd.Value,hiLvlCtrl.basisParams.Value,gndStn.posVec.Value);
-            end
+            fltCtrl.setInitPathVar(vhcl.initPosVecGnd.Value,hiLvlCtrl.basisParams.Value,gndStn.posVec.Value);
+
             
             %%  Set up critical system parameters
             simParams = SIM.simParams;  simParams.setDuration(tFinal,'s');  dynamicCalc = '';
@@ -132,13 +119,12 @@ for i = 1:n
                 thrLength,altitude,flwSpd);
             fprintf(progress)
             %% Run Simulation
-            simWithMonitor('OCTModel','timeStep',2,'minRate',1)
+            simWithMonitor('OCTModel','timeStep',2,'minRate',0.1)
             %%  Log Results
             tsc = signalcontainer(logsout);%Log simulation data
             lap = max(tsc.lapNumS.Data)-1;%# of laps flown 
             
             %% Log data for sweep
-            if simScenario(3) == 1
                 Pow{i,j,k} = tsc.rotPowerSummary(vhcl,env,thr);
                 [Idx1,Idx2,lapCheck] = tsc.getLapIdxs(max(tsc.lapNumS.Data)-1);  ran = Idx1:Idx2;
                 AoA = mean(squeeze(tsc.vhclAngleOfAttack.Data(:,:,ran)));
@@ -146,12 +132,12 @@ for i = 1:n
                 gndNode = squeeze(sqrt(sum(tsc.gndNodeTenVecs.Data.^2,1)))*1e-3;
                 ten = max([max(airNode(ran)) max(gndNode(ran))]);
                 fprintf('Average AoA = %.3f;\t Max Tension = %.1f kN\n\n',AoA,ten);
-            end
+            
             %% Save simulation data
 
                 filename = sprintf(strcat('PathFollow_V-%.2f_Alt-%d_thr-%d.mat'),flwSpd,altitude,thrLength);            
            
-                if simScenario(6)
+                if saveSim
                 save(strcat(fpath,filename),'tsc','vhcl','thr','fltCtrl','env','simParams','LIBRARY','gndStn')
                 end
         end
@@ -159,6 +145,6 @@ for i = 1:n
 end
 
 %% Animate Simulation
-if simScenario(7)
+if animateFlag
     vhcl.animateSim(tsc,2,'tracerDuration',100)
 end
